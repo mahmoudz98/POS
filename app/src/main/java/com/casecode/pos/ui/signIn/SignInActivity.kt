d7f9ef35663196f17c38ae11bc46ab9d694067c0
Mahmoud Abdalhafeez <mah99239@github.com>
2024-02-07 11:04:16 +0200

    Improve logic for SignInActivity
    Signed-off-by: Mahmoud Abdalhafeez <mah99239@github.com>

--- app/src/main/java/com/casecode/pos/ui/signIn/SignInActivity.kt
+++ app/src/main/java/com/casecode/pos/ui/signIn/SignInActivity.kt
@@ -3,7 +3,6 @@ package com.casecode.pos.ui.signIn
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
@@ -48,30 +47,28 @@ class SignInActivity : AppCompatActivity()
       setContentView(binding.root)
       
       
-      launcher =
-         registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK)
-            {
-               lifecycleScope.launch {
-                   viewModel.signInWithIntent(
-                     intent = result.data ?: return@launch)
-               }
-            }
+      launcher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result->
+         if (result.resultCode == Activity.RESULT_OK)
+         {
+            viewModel.signInWithIntent(result.data ?: return@registerForActivityResult)
+            
+            
          }
+      }
       
       binding.signInButton.setSize(SignInButton.SIZE_STANDARD)
       binding.signInButton.setOnClickListener {
          lifecycleScope.launch {
             val signInIntentSender = viewModel.signIn()
-            launcher.launch(IntentSenderRequest.Builder(
-               signInIntentSender ?: return@launch).build())
+            launcher.launch(IntentSenderRequest.Builder(signInIntentSender
+                                                             ?: return@launch).build())
          }
          observerSignInResult()
          
       }
-      binding.textEmployeeLogin.setOnClickListener{
+      binding.textEmployeeLogin.setOnClickListener {
          val employeeLogin = LoginDialogFragment()
-         employeeLogin.show(supportFragmentManager, "Login")
+         employeeLogin.show(supportFragmentManager,"Login")
       }
    }
    
@@ -99,21 +96,19 @@ class SignInActivity : AppCompatActivity()
          {
             is FirebaseAuthResult.Failure ->
             {
-               binding.root.showSnackbar("Sign in Failure", Snackbar.LENGTH_SHORT)
+               binding.root.showSnackbar(getString(R.string.all_error_save),Snackbar.LENGTH_SHORT)
                
             }
             
             is FirebaseAuthResult.SignInFails ->
             {
                Timber.e("SignInFails")
-               binding.root.showSnackbar("Sign in fails", Snackbar.LENGTH_SHORT)
-               
-               
+               binding.root.showSnackbar(getString(R.string.all_error_save),Snackbar.LENGTH_SHORT)
             }
             
             is FirebaseAuthResult.SignInSuccess ->
             {
-               Timber.e("SignInSuccess")
+               Timber.i("SignInSuccess")
                updateUI()
                
                
@@ -144,7 +139,7 @@ class SignInActivity : AppCompatActivity()
             
             else ->
             {
-               binding.root.showSnackbar(getString(R.string.all_error_save), Snackbar.LENGTH_SHORT)
+               binding.root.showSnackbar(getString(R.string.all_error_save),Snackbar.LENGTH_SHORT)
             }
          }
       }
@@ -152,21 +147,16 @@ class SignInActivity : AppCompatActivity()
    
    private fun moveToMainActivity()
    {
-      val intent = Intent(this, MainActivity::class.java)
-      intent.flags =
-         Intent.FLAG_ACTIVITY_CLEAR_TOP // used to clean activity and al activities above it will be removed.
+      val intent = Intent(this,MainActivity::class.java)
+      intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP // used to clean activity and al activities above it will be removed.
       startActivity(intent)
    }
    
    private fun moveToStepperActivity()
    {
-      val intent = Intent(this, StepperActivity::class.java)
+      val intent = Intent(this,StepperActivity::class.java)
       intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
       startActivity(intent)
    }
    
-   companion object
-   {
-      private const val TAG = "SignInActivity"
-   }
 }
