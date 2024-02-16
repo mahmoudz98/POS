386cd953cf26d7d0697a0c8d6fba2d5878131a65
mahmoud <30526294+mah99239@users.noreply.github.com>
2024-02-16 08:53:12 +0200

    refactor and improve performance Sealed Resource

--- domain/src/main/java/com/casecode/domain/utils/Resource.kt
+++ domain/src/main/java/com/casecode/domain/utils/Resource.kt
@@ -1,38 +1,28 @@
 package com.casecode.domain.utils
 
-sealed class Resource<out T>()
-{
-   abstract val data: T?
-   
-    data class Success<T>(override val data: T ) : Resource<T>()
-    data class Error<T>(val message: Any?, override val data: T? = null) : Resource<T>()
-    class Loading<T>(override val data: T? = null) : Resource<T>()
-   data class Empty<T>(val emptyType : EmptyType? = null,val message: Any? = null) :
-      Resource<T>()
-   {
-      override val data: T? = null
-   }
-   
-   companion object
-   {
-      fun <T> success(data: T): Resource<T> = Success(data)
-      fun <T> error(message: Any?): Resource<T> = Error(message, null)
-      fun <T> loading(data: T? = null): Resource<T> = Loading(data)
-      fun <T> empty( emptyType: EmptyType? = null, message: Any? = null): Resource<T> {
-         return Empty(emptyType,message)
-         
-      }
-      
-   }
-   
-   override fun toString(): String
-   {
-      return when (this)
-      {
-         is Success<*> -> "Success[data=$data]"
-         is Error -> "Error[exception=$message]"
-         is Empty -> "Canceled[exception=$message]"
-         is Loading -> "Canceled[exception=$data]"
-      }
-   }
+sealed interface Resource<out T> {
+
+    data class Success<T>( val data: T) : Resource<T>
+    data class Error<T>(val message: Any?,  val data: T? = null) : Resource<T>
+    data object Loading : Resource<Nothing>
+    data class Empty<T>(val emptyType: EmptyType? = null, val message: Any? = null) :
+        Resource<T> {
+         val data: T? = null
+    }
+
+    companion object {
+        inline fun <reified T> success(data: T): Resource<T> = Success(data)
+        inline fun <reified T> error(message: Any?): Resource<T> = Error(
+            message,
+            null
+        )
+
+        fun <T> loading(): Resource<T> = Loading
+        inline fun <reified T> empty(
+            emptyType: EmptyType? = null,
+            message: Any? = null
+        ): Resource<T> =
+            Empty(emptyType, message)
+    }
+
 }
