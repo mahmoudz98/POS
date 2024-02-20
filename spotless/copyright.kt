a61c123a444a5baf600b36d0955abd36cb4125b1
mahmoud <30526294+mah99239@users.noreply.github.com>
2024-02-20 02:21:44 +0200

    update copyright

--- spotless/copyright.kt
+++ spotless/copyright.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright $YEAR Mahmoud Abdalhafeez
+ * Copyright $YEAR Mahmoud Abdalhafeez, Abdelaziz Daoud
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+* Licensed under the MIT License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *     https://opensource.org/licenses/MIT
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
