9e4065674118bea8ef265e45e2dcdcd5a9d922b9
mahmoud <30526294+mah99239@users.noreply.github.com>
2024-02-21 21:15:20 +0200

    Change  logic for inject firebaseAuth to support mock or emulator

--- testing/src/main/java/com/casecode/testing/di/app/TestFirebaseModule.kt
+++ testing/src/main/java/com/casecode/testing/di/app/TestFirebaseModule.kt
@@ -12,37 +12,42 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
+import io.mockk.every
 import io.mockk.mockk
-import javax.inject.Singleton
 
 @Module
 @TestInstallIn(
-   components = [SingletonComponent::class],
-   replaces = [FirebaseModule::class],
-              )
-object TestFirebaseModule
-{
-   private const val HOST = "127.0.0.1"
-   private const val AUTH_PORT = 9099
-   private const val FIRESTORE_PORT = 8080
-   
-   @Singleton
-   @Provides
-   fun provideFirebaseAuthMockk(): FirebaseAuth =
-      Firebase.auth.also { it.useEmulator(HOST,AUTH_PORT) }
-   
-   @Singleton
-   @Provides
-   fun provideFirebaseFirestoreMockk(): FirebaseFirestore =
-      Firebase.firestore.also { it.useEmulator(HOST,FIRESTORE_PORT) }
-   
-   @Provides
-   @Singleton
-   fun provideSignInClient() = mockk<SignInClient>()
-   
-   @Provides
-   fun provideSignInRequest(): BeginSignInRequest
-   {
-      return mockk<BeginSignInRequest>()
-   }
+    components = [SingletonComponent::class],
+    replaces = [FirebaseModule::class],
+)
+object TestFirebaseModule {
+    private const val HOST = "127.0.0.1"
+    private const val AUTH_PORT = 9099
+    private const val FIRESTORE_PORT = 8080
+
+
+    @Provides
+    fun provideFirebaseAuthMockk(): FirebaseAuth {
+        if (Firebase.auth.currentUser != null) {
+            return Firebase.auth.also { it.useEmulator(HOST, AUTH_PORT) }
+        } else {
+            val mockFirebase = mockk<FirebaseAuth>()
+            every { mockFirebase.currentUser?.uid } answers { "Test uid" }
+            return mockFirebase
+        }
+
+
+    }
+
+    @Provides
+    fun provideFirebaseFirestore(): FirebaseFirestore =
+        Firebase.firestore.also { it.useEmulator(HOST, FIRESTORE_PORT) }
+
+    @Provides
+    fun provideSignInClient() = mockk<SignInClient>()
+
+    @Provides
+    fun provideSignInRequest(): BeginSignInRequest {
+        return mockk<BeginSignInRequest>()
+    }
 }
