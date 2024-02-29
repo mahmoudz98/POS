2665db6fd431e64941ce23c9daeb4d315f42b4df
mahmoud <30526294+mah99239@users.noreply.github.com>
2024-02-29 20:37:26 +0200

    reformat

--- app/src/androidTest/java/com/casecode/pos/utils/MatcherExt.kt
+++ app/src/androidTest/java/com/casecode/pos/utils/MatcherExt.kt
@@ -6,11 +6,12 @@ import com.google.android.material.textfield.TextInputLayout
 import org.hamcrest.Description
 import org.hamcrest.TypeSafeMatcher
 
-fun withHint(expected: String) = object : TypeSafeMatcher<View>() {
-   override fun describeTo(description: Description) {
-      description.appendText("TextView or TextInputLayout with hint $expected")
-   }
-   
-   override fun matchesSafely(item: View?) =
-      item is TextInputLayout &&( expected == item.hint || expected == item.error) || item is TextView && expected == item.hint
-}
+fun withHint(expected: String) =
+    object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description) {
+            description.appendText("TextView or TextInputLayout with hint $expected")
+        }
+
+        override fun matchesSafely(item: View?) =
+            item is TextInputLayout && (expected == item.hint || expected == item.error) || item is TextView && expected == item.hint
+    }
