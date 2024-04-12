b0040f83d42c8061886c4904fbb0da66cf33d363
mahmoud <30526294+mah99239@users.noreply.github.com>
2024-04-12 03:54:58 +0200

    use kapt instead of ksp

--- build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
@@ -3,24 +3,21 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
 
-class AndroidHiltConventionPlugin : Plugin<Project>
-{
+class AndroidHiltConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
-                apply("com.google.devtools.ksp")
+                //  apply("com.google.devtools.ksp")
                 apply("dagger.hilt.android.plugin")
+                apply("org.jetbrains.kotlin.kapt")
             }
 
             dependencies {
                 "implementation"(libs.findLibrary("hilt.android").get())
-                "ksp"(libs.findLibrary("hilt.compiler").get())
-               //"kspTest"(libs.findLibrary("hilt.compiler").get())
-              // "kspAndroidTest"(libs.findLibrary("hilt.compiler").get())
-               
+                "kapt"(libs.findLibrary("hilt.compiler").get())
+                // "kspTest"(libs.findLibrary("hilt.compiler").get())
+                // "kspAndroidTest"(libs.findLibrary("hilt.compiler").get())
             }
-
         }
     }
-
 }
