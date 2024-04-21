78f25f5c0961909f5900c25a646e84da6102f661
mahmoud <30526294+mah99239@users.noreply.github.com>
2024-04-21 06:50:22 +0200

    reformat

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,35 +1,25 @@
-
-
 pluginManagement {
-   includeBuild("build-logic")
-   repositories {
-      google {
-         content {
-            includeGroupByRegex("com\\.android.*")
-            includeGroupByRegex("com\\.google.*")
-            includeGroupByRegex("androidx.*")
-         }
-      }
-      mavenCentral()
-      gradlePluginPortal()
-   }
+    includeBuild("build-logic")
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 dependencyResolutionManagement {
-   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-   repositories {
-      google()
-      mavenCentral()
-      maven("https://jitpack.io")
-      
-   }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+
+    }
 }
 rootProject.name = "POS"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-include (":app")
-include (":domain")
+include(":app")
+include(":domain")
 include(":di")
-
-include (":data")
+include(":data")
 include(":testing")
-
