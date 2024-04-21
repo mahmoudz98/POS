71fe97ab38149fbacb4f4c6b339a84b17fd156cf
mahmoud <30526294+mah99239@users.noreply.github.com>
2024-04-21 06:49:58 +0200

    reforamt

--- build-logic/convention/src/main/kotlin/com/casecode/pos/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/casecode/pos/KotlinAndroid.kt
@@ -13,63 +13,55 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 /**
  * Configure base Kotlin with Android options
  */
-internal fun Project.configureKotlinAndroid(
-     commonExtension: CommonExtension<*, *, *, *, *>,
-                                           )
-{
-   commonExtension.apply {
-      compileSdk = Configuration.compileSdk
-      
-      defaultConfig {
-         minSdk = Configuration.minSdk
-      }
-      
-      compileOptions {
-         sourceCompatibility = JavaVersion.VERSION_17
-         targetCompatibility = JavaVersion.VERSION_17
-         isCoreLibraryDesugaringEnabled = true
-      }
-      
-      
-   }
-   
-   configureKotlin()
-   
-   dependencies {
-      add("coreLibraryDesugaring", libs.findLibrary("android.desugarJdkLibs").get())
-   }
+internal fun Project.configureKotlinAndroid(commonExtension: CommonExtension<*, *, *, *, *, *>) {
+    commonExtension.apply {
+        compileSdk = Configuration.compileSdk
+
+        defaultConfig {
+            minSdk = Configuration.minSdk
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+            isCoreLibraryDesugaringEnabled = true
+        }
+    }
+
+    configureKotlin()
+
+    dependencies {
+        add("coreLibraryDesugaring", libs.findLibrary("android.desugarJdkLibs").get())
+    }
 }
 
 /**
  * Configure base Kotlin options for JVM (non-Android)
  */
-internal fun Project.configureKotlinJvm()
-{
-   extensions.configure<JavaPluginExtension> {
-      
-      sourceCompatibility = JavaVersion.VERSION_17
-      targetCompatibility = JavaVersion.VERSION_17
-   }
-   configureKotlin()
+internal fun Project.configureKotlinJvm() {
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    configureKotlin()
 }
 
 /**
  * Configure base Kotlin options
  */
-private fun Project.configureKotlin()
-{
-   tasks.withType<KotlinCompile>().configureEach {
-      kotlinOptions {
-         // Set JVM target to 17
-         jvmTarget = JavaVersion.VERSION_17.toString()
-         // Treat all Kotlin warnings as errors (disabled by default)
-         // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
-         val warningsAsErrors: String? by project
-         allWarningsAsErrors = warningsAsErrors.toBoolean()
-         freeCompilerArgs = freeCompilerArgs + listOf(
-            "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-            
-            )
-      }
-   }
+private fun Project.configureKotlin() {
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            // Set JVM target to 17
+            jvmTarget = JavaVersion.VERSION_17.toString()
+            // Treat all Kotlin warnings as errors (disabled by default)
+            // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
+            val warningsAsErrors: String? by project
+            allWarningsAsErrors = warningsAsErrors.toBoolean()
+            freeCompilerArgs = freeCompilerArgs +
+                listOf(
+                    "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                )
+        }
+    }
 }
