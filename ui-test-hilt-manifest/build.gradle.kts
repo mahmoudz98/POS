36ab294d82ef99374d4405d3f52e2fbde679275e
Mahmoud Abdalhafeez <mahmoud99239@gmail.com>
2024-07-26 10:46:00 +0300

    feat(ui-test-hilt-manifest): add ui-test-hilt-manifest module
    This commit introduces a new module named ui-test-hilt
    -manifest.
    
    The module includes:
    - An AndroidManifest.xml file declaring HiltTestActivity.
    - A build.gradle.kts file configuring the module with necessary plugins and dependencies.
    - A README.md file providing an overview of the module and its dependency graph.
    - A Hilt
    TestActivity class annotated with @AndroidEntryPoint for Hilt dependency injection.

---
+++ ui-test-hilt-manifest/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    alias(libs.plugins.pos.android.library)
+    alias(libs.plugins.pos.hilt)
+}
+android {
+    namespace = "com.casecode.pos.uitesthiltmanifest"
+}
---
+++ ui-test-hilt-manifest/src/main/java/com/casecode/pos/uitesthiltmanifest/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.casecode.pos.uitesthiltmanifest
+
+//import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import androidx.activity.ComponentActivity
+@AndroidEntryPoint
+class HiltTestActivity : ComponentActivity()
