83d6dc329f9bfbd842cb077445249bc1a6b33102
Mahmoud Abdalhafeez <mahmoud99239@gmail.com>
2024-07-30 19:54:42 +0300

    feat(core-data): update PrinterInfo model
    This commit updates the PrinterInfo model to use ConnectionTypeInfo instead of separate fields for connection type, address,
     and port.
    
    This change simplifies the model and makes it more flexible for handling different types of printer connections.

--- core/data/src/main/java/com/casecode/pos/core/data/model/PrinterInfo.kt
+++ core/data/src/main/java/com/casecode/pos/core/data/model/PrinterInfo.kt
@@ -11,9 +11,7 @@ import com.casecode.pos.core.model.data.PrinterInfo
 fun PrinterInfo.asExternalMapper(): Map<String, Any?>{
     return mapOf(
         PRINTER_INFO_NAME_FIELD to this.name,
-        PRINTER_INFO_CONNECTION_TYPE_FIELD to this.connectionType,
-        PRINTER_INFO_ADDRESS_FIELD to  this.address,
-        PRINTER_INFO_PORT_FIELD to this.port,
+        PRINTER_INFO_CONNECTION_TYPE_FIELD to this.connectionTypeInfo,
         PRINTER_INFO_IS_CURRENT_SELECTED_FIELD to this.isCurrentSelected,
         PRINTER_INFO_SIZE_FIELD to this.size,
     )
