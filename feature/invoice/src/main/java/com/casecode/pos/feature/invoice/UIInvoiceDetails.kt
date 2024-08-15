bf9b585cfdead10e9a6e59e9301888d0848f83fe
Mahmoud Abdalhafeez <mahmoud99239@gmail.com>
2024-08-15 17:03:25 +0300

    feat(invoice): remove invoice feature
    This commit removes the invoice feature from the application.
    
    - Deleted all files related to the invoice feature, including navigation,
     UI components, and resource files.

--- feature/invoice/src/main/java/com/casecode/pos/feature/invoice/UIInvoiceDetails.kt
+++
@@ -1,9 +0,0 @@
-package com.casecode.pos.feature.invoice
-
-import com.casecode.pos.core.model.data.users.Invoice
-
-sealed interface UIInvoiceDetails {
-    data object Loading : UIInvoiceDetails
-    data object Empty : UIInvoiceDetails
-    data class Success(val invoice: Invoice) : UIInvoiceDetails
-}
