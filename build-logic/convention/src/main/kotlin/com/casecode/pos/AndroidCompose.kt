ba81cfc9273532295d132e51ef6183c7ce3cccdd
mahmoud <mahmoud99239@gmail.com>
2024-09-06 12:10:25 +0300

    refactor(convention): remove unused import and redundant option
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- build-logic/convention/src/main/kotlin/com/casecode/pos/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/com/casecode/pos/AndroidCompose.kt
@@ -19,10 +19,10 @@ package com.casecode.pos
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 import org.gradle.api.provider.Provider
-import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
+
 
 /**
  * Configure Compose-specific options
@@ -66,6 +66,6 @@ internal fun Project.configureAndroidCompose(commonExtension: CommonExtension<*,
         stabilityConfigurationFile = rootProject.layout.projectDirectory.file("compose_compiler_config.conf")
 */
 
-        enableStrongSkippingMode = true
+
     }
 }
