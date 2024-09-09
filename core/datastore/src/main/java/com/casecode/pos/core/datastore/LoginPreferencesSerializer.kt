156bf5f430f907116a6230c37cf3a75474d00d8d
mahmoud <mahmoud99239@gmail.com>
2024-09-09 19:52:53 +0300

    chore: add license headers
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- core/datastore/src/main/java/com/casecode/pos/core/datastore/LoginPreferencesSerializer.kt
+++ core/datastore/src/main/java/com/casecode/pos/core/datastore/LoginPreferencesSerializer.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.core.datastore
 
 import androidx.datastore.core.CorruptionException
@@ -8,23 +23,23 @@ import java.io.OutputStream
 import javax.inject.Inject
 
 class LoginPreferencesSerializer
-    @Inject
-    constructor() : Serializer<LoginPreferences> {
-        override val defaultValue: LoginPreferences = LoginPreferences.getDefaultInstance()
+@Inject
+constructor() : Serializer<LoginPreferences> {
+    override val defaultValue: LoginPreferences = LoginPreferences.getDefaultInstance()
 
-        override suspend fun readFrom(input: InputStream): LoginPreferences =
-            try {
-                // readFrom is already called on the data store background thread
-                LoginPreferences.parseFrom(input)
-            } catch (exception: InvalidProtocolBufferException) {
-                throw CorruptionException("Cannot read proto.", exception)
-            }
+    override suspend fun readFrom(input: InputStream): LoginPreferences =
+        try {
+            // readFrom is already called on the data store background thread
+            LoginPreferences.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
 
-        override suspend fun writeTo(
-            t: LoginPreferences,
-            output: OutputStream,
-        ) {
-            // writeTo is already called on the data store background thread
+    override suspend fun writeTo(
+        t: LoginPreferences,
+        output: OutputStream,
+    ) {
+        // writeTo is already called on the data store background thread
         t.writeTo(output)
     }
 }
