9bc2d8c080d1b568c9e3af9c1296c423980d6f78
mahmoud <mahmoud99239@gmail.com>
2024-09-10 19:21:59 +0300

    test: add license to domain test classes
    This commit adds license information to the following domain test classes:
    
    - AddBranchBusinessUseCaseTest
    - GetBusiness
    UseCaseTest
    - DeleteEmployeeUseCaseTest
    
    The license information includes the copyright holder, year, and the MIT License terms.
    
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- core/domain/src/test/java/com/casecode/pos/core/domain/usecase/AddBranchBusinessUseCaseTest.kt
+++ core/domain/src/test/java/com/casecode/pos/core/domain/usecase/AddBranchBusinessUseCaseTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.core.domain.usecase
 
 import com.casecode.pos.core.domain.utils.AddBranchBusinessResult
@@ -22,9 +37,7 @@ class AddBranchBusinessUseCaseTest {
         val inputBranch = Branch(1, "Test Branch", "1234567890")
         val result = addBranchBusinessUseCaseTest(inputBranch)
         assert(result is AddBranchBusinessResult.Success)
-
     }
-
 
     @Test
     fun `add branch when Repository error then return error`() = runTest {
@@ -32,7 +45,6 @@ class AddBranchBusinessUseCaseTest {
         val inputBranch = Branch(1, "Test Branch", "1234567890")
         val result = addBranchBusinessUseCaseTest(inputBranch)
         assert(result is AddBranchBusinessResult.Error)
-
     }
 
     @Test
@@ -40,9 +52,5 @@ class AddBranchBusinessUseCaseTest {
         val inputBranch = Branch(-1, "", "1234567890")
         val result = addBranchBusinessUseCaseTest(inputBranch)
         assert(result is AddBranchBusinessResult.Error)
-
-
     }
-
-
 }
