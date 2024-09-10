c0119c32047c2954482999bff089eaeab7a9fdd4
mahmoud <mahmoud99239@gmail.com>
2024-09-10 19:23:08 +0300

    style: add license headers to benchmark module
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- benchmark/src/main/java/com/casecode/pos/benchmark/ExampleStartupBenchmark.kt
+++ benchmark/src/main/java/com/casecode/pos/benchmark/ExampleStartupBenchmark.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.benchmark
 
 import androidx.benchmark.macro.BaselineProfileMode.Disable
@@ -7,7 +22,6 @@ import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.StartupMode.COLD
 import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Rule
 import org.junit.Test
