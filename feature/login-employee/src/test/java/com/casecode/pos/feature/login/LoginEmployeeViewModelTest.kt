3382a6793431676bbabc608c576fca13d72e6001
mahmoud <mahmoud99239@gmail.com>
2024-09-10 18:06:44 +0300

    refactor(login): move LoginEmployeeViewModel to login package
    This commit moves the `LoginEmployeeViewModel` and its associated UI state (`LoginEmployeeUiState
    `) from the `login_employee` package to the `login` package. It also adds a license header to the test file.
    
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- feature/login-employee/src/test/java/com/casecode/pos/feature/login/LoginEmployeeViewModelTest.kt
+++ feature/login-employee/src/test/java/com/casecode/pos/feature/login/LoginEmployeeViewModelTest.kt
@@ -1,7 +1,23 @@
-package com.casecode.pos.feature.login_employee
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.casecode.pos.feature.login
 
 import com.casecode.pos.core.testing.base.BaseTest
 import com.casecode.pos.core.testing.util.MainDispatcherRule
+import com.casecode.pos.feature.login.employee.LoginEmployeeViewModel
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import kotlin.test.Test
@@ -18,7 +34,7 @@ class LoginEmployeeViewModelTest : BaseTest() {
         viewModel = LoginEmployeeViewModel(networkMonitor, accountService)
     }
 
-    @org.junit.Test
+    @Test
     fun testLoginByEmployee_whenOffline_showsNetworkError() =
         runTest {
             networkMonitor.setConnected(false)
