63cf17ce53f89997e6376d67d7002a1fd1ee2aa1
mahmoud <mahmoud99239@gmail.com>
2024-10-10 01:14:21 +0300

    style: add license to EmployeeEmptyScreen
    This commit adds license information to the `EmployeeEmptyScreen` composable function.
    
    The license information includes the copyright holder, year, and the MIT License terms.
    
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- core/ui/src/main/java/com/casecode/pos/core/ui/EmployeeEmptyScreen.kt
+++ core/ui/src/main/java/com/casecode/pos/core/ui/EmployeeEmptyScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.core.ui
 
 import androidx.compose.foundation.Image
@@ -22,9 +37,9 @@ import androidx.compose.ui.unit.dp
 fun EmployeeEmptyScreen(modifier: Modifier = Modifier) {
     Column(
         modifier =
-            modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState()),
+        modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
