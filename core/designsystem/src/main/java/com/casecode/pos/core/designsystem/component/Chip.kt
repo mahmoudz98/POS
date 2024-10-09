33e3694c473ce602e8e9bdf501556b71b416723d
mahmoud <mahmoud99239@gmail.com>
2024-10-10 01:36:57 +0300

    feat(designsystem): add `PosElevatedFilterChip` and `PosInputChip` components
    This commit introduces two new chip components to the design system:
    
    - `PosElevatedFilterChip`: An elevated filter chip with a leading checked icon and text content slot.
    - `PosInputChip`: An input chip with a leading checked icon, avatar icon, and text content slot.
    
    This commit also refactors the `PosFilterChip` to update selected chip colors.
    
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- core/designsystem/src/main/java/com/casecode/pos/core/designsystem/component/Chip.kt
+++ core/designsystem/src/main/java/com/casecode/pos/core/designsystem/component/Chip.kt
@@ -15,11 +15,14 @@
  */
 package com.casecode.pos.core.designsystem.component
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ElevatedFilterChip
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Text
@@ -31,7 +34,7 @@ import com.casecode.pos.core.designsystem.icon.PosIcons
 import com.casecode.pos.core.designsystem.theme.POSTheme
 
 /**
- * Now in Android filter chip with included leading checked icon as well as text content slot.
+ * POS filter chip with included leading checked icon as well as text content slot.
  *
  * @param selected Whether the chip is currently checked.
  * @param onSelectedChange Called when the user clicks the chip and toggles checked.
@@ -107,8 +110,160 @@ fun PosFilterChip(
                 alpha = POSChipDefaults.DISABLED_CHIP_CONTENT_ALPHA,
             ),
             selectedContainerColor = MaterialTheme.colorScheme.primaryContainer,
-            selectedLabelColor = MaterialTheme.colorScheme.onBackground,
-            selectedLeadingIconColor = MaterialTheme.colorScheme.onBackground,
+            selectedLabelColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            selectedLeadingIconColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        ),
+    )
+}
+
+/**
+ * POS Elevated filter chip with included leading checked icon as well as text content slot.
+ *
+ * @param selected Whether the chip is currently checked.
+ * @param onSelectedChange Called when the user clicks the chip and toggles checked.
+ * @param modifier Modifier to be applied to the chip.
+ * @param enabled Controls the enabled state of the chip. When `false`, this chip will not be
+ * clickable and will appear disabled to accessibility services.
+ * @param label The text label content.
+ */
+@Composable
+fun PosElevatedFilterChip(
+    selected: Boolean,
+    onSelectedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    label: @Composable () -> Unit,
+) {
+    ElevatedFilterChip(
+        selected = selected,
+        onClick = { onSelectedChange(!selected) },
+        label = {
+            ProvideTextStyle(value = MaterialTheme.typography.labelMedium) {
+                label()
+            }
+        },
+        modifier = modifier,
+        enabled = enabled,
+        trailingIcon = if (selected) {
+            {
+                Icon(
+                    imageVector = PosIcons.Check,
+                    contentDescription = null,
+                )
+            }
+        } else {
+            null
+        },
+        colors =
+        FilterChipDefaults.filterChipColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainer,
+            labelColor = MaterialTheme.colorScheme.onBackground,
+            iconColor = MaterialTheme.colorScheme.onBackground,
+            disabledContainerColor =
+            if (selected) {
+                MaterialTheme.colorScheme.onBackground.copy(
+                    alpha = POSChipDefaults.DISABLED_CHIP_CONTAINER_ALPHA,
+                )
+            } else {
+                MaterialTheme.colorScheme.surface
+            },
+            disabledLabelColor =
+            MaterialTheme.colorScheme.onBackground.copy(
+                alpha = POSChipDefaults.DISABLED_CHIP_CONTENT_ALPHA,
+            ),
+            disabledTrailingIconColor =
+            MaterialTheme.colorScheme.onBackground.copy(
+                alpha = POSChipDefaults.DISABLED_CHIP_CONTENT_ALPHA,
+            ),
+            selectedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+            selectedLabelColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            selectedTrailingIconColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        ),
+    )
+}
+
+/**
+ * POS filter chip with included leading checked icon as well as text content slot.
+ *
+ * @param selected Whether the chip is currently checked.
+ * @param onSelectedChange Called when the user clicks the chip and toggles checked.
+ * @param modifier Modifier to be applied to the chip.
+ * @param enabled Controls the enabled state of the chip. When `false`, this chip will not be
+ * clickable and will appear disabled to accessibility services.
+ * @param label The text label content.
+ */
+@Composable
+fun PosInputChip(
+    selected: Boolean,
+    onSelectedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    label: @Composable () -> Unit,
+) {
+    InputChip(
+        selected = selected,
+        onClick = { onSelectedChange(!selected) },
+        label = {
+            ProvideTextStyle(value = MaterialTheme.typography.labelSmall) {
+                label()
+            }
+        },
+        modifier = modifier,
+        enabled = enabled,
+        avatar = {
+            Icon(imageVector = PosIcons.Track, contentDescription = null)
+        },
+        leadingIcon =
+        if (selected) {
+            {
+                Icon(
+                    imageVector = PosIcons.Close,
+                    contentDescription = null,
+                )
+            }
+        } else {
+            null
+        },
+        shape = CircleShape,
+        border =
+        FilterChipDefaults.filterChipBorder(
+            enabled = enabled,
+            selected = selected,
+            borderColor = MaterialTheme.colorScheme.onBackground,
+            selectedBorderColor = MaterialTheme.colorScheme.onBackground,
+            disabledBorderColor =
+            MaterialTheme.colorScheme.onBackground.copy(
+                alpha = POSChipDefaults.DISABLED_CHIP_CONTENT_ALPHA,
+            ),
+            disabledSelectedBorderColor =
+            MaterialTheme.colorScheme.onBackground.copy(
+                alpha = POSChipDefaults.DISABLED_CHIP_CONTENT_ALPHA,
+            ),
+            selectedBorderWidth = POSChipDefaults.ChipBorderWidth,
+        ),
+        colors =
+        FilterChipDefaults.filterChipColors(
+            labelColor = MaterialTheme.colorScheme.onBackground,
+            iconColor = MaterialTheme.colorScheme.onBackground,
+            disabledContainerColor =
+            if (selected) {
+                MaterialTheme.colorScheme.onBackground.copy(
+                    alpha = POSChipDefaults.DISABLED_CHIP_CONTAINER_ALPHA,
+                )
+            } else {
+                Color.Transparent
+            },
+            disabledLabelColor =
+            MaterialTheme.colorScheme.onBackground.copy(
+                alpha = POSChipDefaults.DISABLED_CHIP_CONTENT_ALPHA,
+            ),
+            disabledLeadingIconColor =
+            MaterialTheme.colorScheme.onBackground.copy(
+                alpha = POSChipDefaults.DISABLED_CHIP_CONTENT_ALPHA,
+            ),
+            selectedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+            selectedLabelColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            selectedLeadingIconColor = MaterialTheme.colorScheme.onPrimaryContainer,
         ),
     )
 }
@@ -117,9 +272,21 @@ fun PosFilterChip(
 @Composable
 fun ChipPreview() {
     POSTheme {
-        PosBackground(modifier = Modifier.size(80.dp, 20.dp)) {
-            PosFilterChip(selected = true, onSelectedChange = {}) {
-                Text("Chip")
+        PosBackground(modifier = Modifier.size(300.dp, 400.dp)) {
+            Column {
+                PosFilterChip(selected = true, onSelectedChange = {}) {
+                    Text("PosFilterChip")
+                }
+                PosInputChip(selected = true, onSelectedChange = {}) {
+                    Text("PosInputChip")
+                }
+
+                PosElevatedFilterChip(selected = true, onSelectedChange = {}) {
+                    Text("ElevatedFilterChip")
+                }
+                PosElevatedFilterChip(selected = false, onSelectedChange = {}) {
+                    Text("ElevatedFilterChip")
+                }
             }
         }
     }
