fea4aa46c1d9fd0d5636b1e0510b9dad45c0aba4
mahmoud <mahmoud99239@gmail.com>
2024-10-10 01:11:58 +0300

    style: add license to ActivityUtils
    This commit adds a license header to the `ActivityUtils.kt` file.
    
    The license information includes the copyright holder, year, and the MIT License terms.
    
    Additionally, this commit updates the `moveToSignInActivity` function to use the correct package name for the `SignInActivity` class:
    `com.casecode.pos.feature.signin.SignInActivity`.
    
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- core/ui/src/main/java/com/casecode/pos/core/ui/ActivityUtils.kt
+++ core/ui/src/main/java/com/casecode/pos/core/ui/ActivityUtils.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.core.ui
 
 import android.app.Activity
@@ -6,7 +21,7 @@ import android.content.ContextWrapper
 import android.content.Intent
 
 fun moveToSignInActivity(context: Context) {
-    val intent = Intent(context, Class.forName("com.casecode.pos.ui.signIn.SignInActivity"))
+    val intent = Intent(context, Class.forName("com.casecode.pos.feature.signin.SignInActivity"))
     intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
     context.startActivity(intent)
     context.findActivity().finish()
