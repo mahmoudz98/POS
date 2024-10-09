3dd82c97cd5be5c5dbc3d0c4d0ac85b21e1803ee
mahmoud <mahmoud99239@gmail.com>
2024-10-10 01:06:36 +0300

    refactor(domain): rename InvoiceUseCaseTest to AddInvoiceUseCaseTest and update tests
    This commit renames `InvoiceUseCaseTest` to `AddInvoiceUseCaseTest` and updates the corresponding tests to focus solely on the `AddInvoiceUseCase`.
    
    - Renamed `InvoiceUseCaseTest` to `AddInvoiceUseCaseTest`.
    - Removed tests related to `GetInvoicesUseCase`.
    - Updated imports to remove unused classes.
    - Replaced `MatcherAssert.assertThat` with `assertEquals` for assertions.
    - Used `itemsTestData` instead of `fakeItems` for test data.
    - Updated test cases to reflect the changes in the class name and test data.
    
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- core/domain/src/test/java/com/casecode/pos/core/domain/usecase/AddInvoiceUseCaseTest.kt
+++ core/domain/src/test/java/com/casecode/pos/core/domain/usecase/AddInvoiceUseCaseTest.kt
@@ -17,43 +17,36 @@ package com.casecode.pos.core.domain.usecase
 
 import com.casecode.pos.core.domain.utils.Resource
 import com.casecode.pos.core.model.data.users.Item
+import com.casecode.pos.core.testing.data.itemsTestData
 import com.casecode.pos.core.testing.repository.TestInvoiceRepository
 import com.casecode.pos.core.testing.util.MainDispatcherRule
 import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.test.runTest
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.MatcherAssert
 import org.junit.Rule
-import org.junit.Test
+import kotlin.test.Test
+import kotlin.test.assertEquals
 import com.casecode.pos.core.data.R.string as stringData
 import com.casecode.pos.core.domain.R.string as stringDomain
 
-class InvoiceUseCaseTest {
+class AddInvoiceUseCaseTest {
+
     @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
 
     // Subject under test
     private val testInvoiceRepository = TestInvoiceRepository()
     private val addInvoiceUseCase = AddInvoiceUseCase(testInvoiceRepository)
-    private val getInvoicesUseCase = GetInvoicesUseCase(testInvoiceRepository)
-    var fakeItems =
-        arrayListOf(
-            Item("item #1", 1.0, 23.0, "1234567899090", "EA", "www.image1.png"),
-            Item("item #2", 3.0, 421312.0, "1555567899090", "EA", "www.image2.png"),
-            Item("item #2", 3.0, 0.0, "1200", "EA", "www.image2.png"),
-        )
 
     @Test
     fun addInvoiceUseCase_whenHasInvoices_returnMessageAddedInvoice() =
         runTest {
             // Given
-            val items = fakeItems
             // When
-            val result = addInvoiceUseCase(items).last()
+            val result = addInvoiceUseCase(itemsTestData).last()
             // Then
-            MatcherAssert.assertThat(
+            assertEquals(
                 result,
-                `is`(Resource.success(stringData.core_data_add_invoice_successfully)),
+                (Resource.success(stringData.core_data_add_invoice_successfully)),
             )
         }
 
@@ -65,9 +58,9 @@ class InvoiceUseCaseTest {
             // When
             val result = addInvoiceUseCase(items).last()
             // Then
-            MatcherAssert.assertThat(
+            assertEquals(
                 result,
-                `is`(Resource.empty(message = stringDomain.core_domain_invoice_items_empty)),
+                (Resource.empty(message = stringDomain.core_domain_invoice_items_empty)),
             )
         }
 
@@ -75,49 +68,14 @@ class InvoiceUseCaseTest {
     fun addInvoiceUseCase_hasError_returnMessageError() =
         runTest {
             // Given
-            val items = fakeItems
+            val items = itemsTestData
             // When
             testInvoiceRepository setReturnError true
             val result = addInvoiceUseCase(items).last()
             // Then
-            MatcherAssert.assertThat(
+            assertEquals(
                 result,
-                `is`(Resource.error(stringData.core_data_add_invoice_failure)),
+                (Resource.error(stringData.core_data_add_invoice_failure)),
             )
-        }
-
-    @Test
-    fun getInvoiceUseCase_hasInvoices_returnListOfInvoices() =
-        runTest {
-            // When
-            val result = getInvoicesUseCase().last()
-            // Then
-            MatcherAssert.assertThat(
-                result,
-                `is`(Resource.success(testInvoiceRepository.fakeInvoiceGroup)),
-            )
-        }
-
-    @Test
-    fun getInvoiceUseCase_whenHasError_returnError() =
-        runTest {
-            // When
-            testInvoiceRepository setReturnError true
-            val result = getInvoicesUseCase().last()
-            // Then
-            MatcherAssert.assertThat(
-                result,
-                `is`(Resource.error(stringData.core_data_get_invoice_failure)),
-            )
-        }
-
-    @Test
-    fun getInvoiceUseCase_whenEmptyInvoices_returnEmpty() =
-        runTest {
-            // When
-            testInvoiceRepository setReturnEmpty true
-            val result = getInvoicesUseCase().last()
-            // Then
-            MatcherAssert.assertThat(result, `is`(Resource.empty()))
         }
 }
