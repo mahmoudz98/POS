1361c2184f66856307ba667437b8feabc96b4b12
mahmoud <mahmoud99239@gmail.com>
2024-10-10 01:18:46 +0300

    feat: enable power assert in android library modules
    This commit enables the Kotlin Power Assert compiler plugin for Android library modules to provide more detailed and informative assertion messages during testing.
    
    - Applied the `org.jetbrains.kotlin.plugin.power-assert` plugin to Android library modules.
    - Configured Power Assert using the `PowerAssertGradleExtension`.
    
    This change improves the debugging experience by enhancing error messages with contextual information, making it easier to identify and fix test failures.
    
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -1,9 +1,11 @@
+
 import com.android.build.api.variant.LibraryAndroidComponentsExtension
 import com.android.build.gradle.LibraryExtension
 import com.casecode.pos.Configuration
 import com.casecode.pos.configureFlavors
 import com.casecode.pos.configureGradleManagedDevices
 import com.casecode.pos.configureKotlinAndroid
+import com.casecode.pos.configurePowerAssert
 import com.casecode.pos.configurePrintApksTask
 import com.casecode.pos.disableUnnecessaryAndroidTests
 import com.casecode.pos.libs
@@ -12,14 +14,19 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.kotlin
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.powerassert.gradle.PowerAssertGradleExtension
+
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
                 apply("com.android.library")
                 apply("org.jetbrains.kotlin.android")
                 apply("pos.android.lint")
+                apply("org.jetbrains.kotlin.plugin.power-assert")
             }
 
             extensions.configure<LibraryExtension> {
@@ -28,6 +35,7 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
                 defaultConfig.testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
                 testOptions.animationsDisabled = true
+
                 configureFlavors(this)
                 configureGradleManagedDevices(this)
                 resourcePrefix = path
@@ -41,6 +49,9 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             extensions.configure<LibraryAndroidComponentsExtension> {
                 configurePrintApksTask(this)
                 disableUnnecessaryAndroidTests(target)
+            }
+            extensions.configure<PowerAssertGradleExtension> {
+                configurePowerAssert()
             }
 
             dependencies {
