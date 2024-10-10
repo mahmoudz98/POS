8320af7c471ba0303dd594720f8fa1cb99685b52
mahmoud <mahmoud99239@gmail.com>
2024-10-10 17:25:52 +0300

    refactor(stepper): update BranchDialog content
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- feature/stepper/src/main/java/com/casecode/pos/feature/stepper/branches/BranchDialog.kt
+++ feature/stepper/src/main/java/com/casecode/pos/feature/stepper/branches/BranchDialog.kt
@@ -1,16 +1,24 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.feature.stepper.branches
 
 import android.content.Context
 import android.telephony.TelephonyManager
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,21 +27,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.casecode.pos.core.designsystem.component.PosBackground
-import com.casecode.pos.core.designsystem.component.PosOutlinedTextField
+import com.casecode.pos.core.designsystem.component.PosTextButton
 import com.casecode.pos.core.designsystem.theme.POSTheme
 import com.casecode.pos.core.model.data.users.Branch
+import com.casecode.pos.core.ui.BranchDialogContent
+import com.casecode.pos.core.ui.DevicePreviews
 import com.casecode.pos.core.ui.validatePhoneNumber
 import com.casecode.pos.feature.stepper.StepperBusinessViewModel
 import com.casecode.pos.core.ui.R.string as uiString
@@ -91,12 +98,12 @@ private fun BranchDialog(
             onDismissRequest()
         }
     }
+
     AlertDialog(
         properties = DialogProperties(usePlatformDefaultWidth = false),
         modifier = modifier.widthIn(max = configuration.screenWidthDp.dp - 80.dp),
         onDismissRequest = {
             focusRequester.freeFocus()
-
             keyboardController?.hide()
             onDismissRequest()
         },
@@ -106,69 +113,41 @@ private fun BranchDialog(
             )
         },
         text = {
-            Column {
-                PosOutlinedTextField(
-                    value = branchName,
-                    onValueChange = {
-                        branchName = it
-                        nameError = it.isEmpty()
-                    },
-                    isError = nameError,
-                    label = stringResource(id = uiString.core_ui_branch_name_hint),
-                    keyboardOptions =
-                        KeyboardOptions(
-                            keyboardType = KeyboardType.Text,
-                            imeAction = ImeAction.Next,
-                        ),
-                    supportingText = if (nameError) stringResource(uiString.core_ui_error_branch_name_empty) else null,
-                    modifier =
-                        Modifier
-                            .fillMaxWidth()
-                            .focusRequester(focusRequester),
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                PosOutlinedTextField(
-                    value = phoneNumber,
-                    onValueChange = {
-                        phoneNumber = it
-                        phoneError = validatePhoneNumber(it, countryIsoCode)
-                    },
-                    isError = phoneError != null,
-                    supportingText = phoneError?.let { stringResource(it) },
-                    keyboardActions =
-                        KeyboardActions(
-                            onDone = {
-                                onDoneClickTriggered()
-                            },
-                        ),
-                    keyboardOptions =
-                        KeyboardOptions(
-                            keyboardType = KeyboardType.Phone,
-                            imeAction = ImeAction.Done,
-                    ),
-                    label = stringResource(id = uiString.core_ui_hint_phone_number_branch),
-                    modifier = Modifier.fillMaxWidth(),
-                )
-            }
+            BranchDialogContent(
+                branchName = branchName,
+                hasNameError = nameError,
+                onNameChange = {
+                    branchName = it
+                    nameError = it.isEmpty()
+                },
+                phoneNumber = phoneNumber,
+                onPhoneNumberChange = {
+                    phoneNumber = it
+                    phoneError = validatePhoneNumber(it, countryIsoCode)
+                },
+                phoneError = phoneError,
+                focusRequester = focusRequester,
+                onDoneClickTriggered = onDoneClickTriggered,
+            )
         },
         confirmButton = {
-            Button(
+            PosTextButton(
                 onClick = {
                     onDoneClickTriggered()
                 },
             ) {
                 Text(
                     text =
-                        stringResource(
-                            if (isUpdate) uiString.core_ui_update_branch_button_text else uiString.core_ui_add_branch_button_text,
-                        ),
+                    stringResource(
+                        if (isUpdate) uiString.core_ui_update_branch_button_text else uiString.core_ui_add_branch_button_text,
+                    ),
                 )
             }
         },
     )
 }
 
-@com.casecode.pos.core.ui.DevicePreviews
+@DevicePreviews
 @Composable
 fun BranchDialogPreview() {
     POSTheme {
@@ -184,17 +163,17 @@ fun BranchDialogPreview() {
     }
 }
 
-@com.casecode.pos.core.ui.DevicePreviews
+@DevicePreviews
 @Composable
 fun BranchUpdateDialogPreview() {
     POSTheme {
         PosBackground {
             BranchDialog(
                 branchSelected =
-                    Branch(
-                        branchCode = 1,
-                        branchName = "branch1",
-                        phoneNumber = "0000000000",
+                Branch(
+                    branchCode = 1,
+                    branchName = "branch1",
+                    phoneNumber = "0000000000",
                 ),
                 countryIsoCode = "",
                 isUpdate = true,
