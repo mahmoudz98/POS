4914f4204f3385bd5a747a909ba47efe5d01b5cf
mahmoud <mahmoud99239@gmail.com>
2024-10-10 17:26:39 +0300

    style: add license headers
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- feature/stepper/src/main/java/com/casecode/pos/feature/stepper/branches/BranchList.kt
+++ feature/stepper/src/main/java/com/casecode/pos/feature/stepper/branches/BranchList.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.feature.stepper.branches
 
 import androidx.compose.foundation.clickable
@@ -37,9 +52,9 @@ internal fun BranchesList(
         val scrollableState = rememberLazyListState()
         LazyColumn(
             modifier =
-                modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 8.dp),
+            modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp),
             state = scrollableState,
         ) {
             branches.forEach { branch ->
@@ -54,11 +69,11 @@ internal fun BranchesList(
             )
         scrollableState.DraggableScrollbar(
             modifier =
-                Modifier
-                    .fillMaxHeight()
-                    .windowInsetsPadding(WindowInsets.systemBars)
-                    .padding(horizontal = 2.dp)
-                    .align(Alignment.CenterEnd),
+            Modifier
+                .fillMaxHeight()
+                .windowInsetsPadding(WindowInsets.systemBars)
+                .padding(horizontal = 2.dp)
+                .align(Alignment.CenterEnd),
             state = scrollbarState,
             orientation = Orientation.Vertical,
             onThumbMoved =
--- feature/stepper/src/main/java/com/casecode/pos/feature/stepper/branches/BranchesScreen.kt
+++ feature/stepper/src/main/java/com/casecode/pos/feature/stepper/branches/BranchesScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.feature.stepper.branches
 
 import androidx.compose.foundation.layout.Arrangement
@@ -74,9 +89,9 @@ internal fun BranchesScreen(
 ) {
     Column(
         modifier =
-            modifier
-                .fillMaxSize()
-                .padding(8.dp),
+        modifier
+            .fillMaxSize()
+            .padding(8.dp),
         verticalArrangement = Arrangement.SpaceBetween,
     ) {
         Column(modifier = Modifier.weight(1f)) {
@@ -85,9 +100,9 @@ internal fun BranchesScreen(
         }
         Row(
             modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
+            Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
         ) {
             PosTextButton(
@@ -110,8 +125,8 @@ internal fun BranchesScreen(
                     )
                 },
                 modifier =
-                    Modifier
-                        .wrapContentSize(),
+                Modifier
+                    .wrapContentSize(),
             )
         }
     }
