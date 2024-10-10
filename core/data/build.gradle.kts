162201726b49e893431e7d0aa6624d5e70d05372
mahmoud <mahmoud99239@gmail.com>
2024-10-10 17:32:14 +0300

    style: add license headers to core/data module
    This commit adds license information to the core/data module's `build.gradle.kts` file.
    It also applies the Jacoco plugin for code coverage reporting.
    Additionally, some dependency declarations are modified:
    
    - Adds core.firebaseServices as an implementation dependency.
    - Removes Firebase library, secrets, and billing KTX dependencies.
    - Removes testImplementation dependency on Hamcrest library and mockk agent.
    - Removes unnecessary buildFeatures declaration for buildConfig.
    
    The license information includes the copyright holder, year, and the MIT License terms.
    
    Signed-off-by: mahmoud <mahmoud99239@gmail.com>

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,8 +1,22 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 plugins {
     alias(libs.plugins.pos.android.library)
+    alias(libs.plugins.pos.android.library.jacoco)
     alias(libs.plugins.pos.hilt)
-    alias(libs.plugins.pos.android.firebase.library)
-    alias(libs.plugins.secrets)
 }
 android {
     namespace = "com.casecode.pos.core.data"
@@ -13,34 +27,27 @@ android {
             isReturnDefaultValues = true
         }
     }
-    buildFeatures {
-        buildConfig = true
-    }
+
 }
-secrets {
-    defaultPropertiesFileName = "secrets.defaults.properties"
-}
+
 dependencies {
 
     api(projects.core.domain)
     api(projects.core.common)
     api(projects.core.datastore)
+    implementation(projects.core.firebaseServices)
 
-    // Coroutines
     implementation(libs.coroutines.android)
 
-    implementation(libs.googleid)
-    implementation(libs.billing.ktx)
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
+    // implementation(libs.billing.ktx)
 
     implementation(libs.hilt.android)
     implementation(libs.zxing.generate.barcode)
 
-    testImplementation(libs.test.hamcrest)
-    testCompileOnly(libs.test.hamcrest.library)
+
     testImplementation(projects.core.testing)
     testImplementation(libs.coroutines.test)
-    implementation(libs.mockk.agent)
 
 }
