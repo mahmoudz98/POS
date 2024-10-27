79eac4081887f671fdf9a7405b7aa097fcab9ea0
Mahmoud AbdAlhafeez <mahmoudabdalhafeez98@gmail.com>
2024-10-27 17:27:25 +0300

    refactor(core/data): update printer repository
    This commit updates the `PrinterRepositoryImpl` and `PrinterInfo` classes to use the FirestoreService and external data models for better data handling.
    
    - `PrinterRepositoryImpl` now uses FirestoreService for database operations and AuthRepository for authentication checks.
    - `PrinterInfo` now includes external mapper functions for converting between data models and external representations.
    - `PrinterConnectionInfo` is introduced for handling different connection types (Bluetooth, TCP, USB).
    - Data models are updated to reflect the new structure and use external mapper functions for data conversion.

--- core/data/src/main/java/com/casecode/pos/core/data/model/PrinterInfo.kt
+++ core/data/src/main/java/com/casecode/pos/core/data/model/PrinterInfo.kt
@@ -1,15 +1,84 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@file:Suppress("UNCHECKED_CAST")
+
 package com.casecode.pos.core.data.model
 
-import com.casecode.pos.core.data.utils.PRINTER_INFO_CONNECTION_TYPE_FIELD
-import com.casecode.pos.core.data.utils.PRINTER_INFO_IS_CURRENT_SELECTED_FIELD
-import com.casecode.pos.core.data.utils.PRINTER_INFO_NAME_FIELD
-import com.casecode.pos.core.data.utils.PRINTER_INFO_SIZE_FIELD
+import com.casecode.pos.core.firebase.services.PRINTER_INFO_ADDRESS_FIELD
+import com.casecode.pos.core.firebase.services.PRINTER_INFO_CONNECTION_TYPE_FIELD
+import com.casecode.pos.core.firebase.services.PRINTER_INFO_IS_CURRENT_SELECTED_FIELD
+import com.casecode.pos.core.firebase.services.PRINTER_INFO_NAME_DEVICE_FIELD
+import com.casecode.pos.core.firebase.services.PRINTER_INFO_NAME_FIELD
+import com.casecode.pos.core.firebase.services.PRINTER_INFO_PORT_FIELD
+import com.casecode.pos.core.firebase.services.PRINTER_INFO_SIZE_FIELD
+import com.casecode.pos.core.firebase.services.PRINTER_INFO_USB_NAME_DEVICE_FIELD
+import com.casecode.pos.core.model.data.PrinterConnectionInfo
 import com.casecode.pos.core.model.data.PrinterInfo
+import com.google.firebase.firestore.DocumentSnapshot
 
 fun PrinterInfo.asExternalMapper(): Map<String, Any?> =
     mapOf(
         PRINTER_INFO_NAME_FIELD to this.name,
-        PRINTER_INFO_CONNECTION_TYPE_FIELD to this.connectionTypeInfo,
+        PRINTER_INFO_CONNECTION_TYPE_FIELD to this.connectionTypeInfo.asExternalMapper(),
         PRINTER_INFO_IS_CURRENT_SELECTED_FIELD to this.isDefaultPrint,
         PRINTER_INFO_SIZE_FIELD to this.widthPaper,
     )
+
+private fun PrinterConnectionInfo.asExternalMapper(): Map<String, Any?> =
+    when (this) {
+        is PrinterConnectionInfo.Bluetooth -> {
+            mapOf(
+                PRINTER_INFO_NAME_DEVICE_FIELD to this.name,
+                PRINTER_INFO_ADDRESS_FIELD to this.macAddress,
+            )
+        }
+
+        is PrinterConnectionInfo.Tcp -> {
+            mapOf(
+                PRINTER_INFO_ADDRESS_FIELD to this.ipAddress,
+                PRINTER_INFO_PORT_FIELD to this.port,
+            )
+        }
+
+        is PrinterConnectionInfo.Usb -> {
+            mapOf(
+                PRINTER_INFO_USB_NAME_DEVICE_FIELD to this.usbDeviceName,
+            )
+        }
+    }
+
+fun DocumentSnapshot.asExternalModel(): PrinterInfo = PrinterInfo(
+    name = this[PRINTER_INFO_NAME_FIELD] as String,
+    connectionTypeInfo = (this[PRINTER_INFO_CONNECTION_TYPE_FIELD] as Map<String, Any?>).asExternalConnectionModel(),
+    isDefaultPrint = this[PRINTER_INFO_IS_CURRENT_SELECTED_FIELD] as Boolean,
+    widthPaper = this[PRINTER_INFO_SIZE_FIELD] as String,
+)
+
+private fun Map<String, Any?>.asExternalConnectionModel(): PrinterConnectionInfo {
+    if (this[PRINTER_INFO_USB_NAME_DEVICE_FIELD] != null) {
+        return PrinterConnectionInfo.Usb(this[PRINTER_INFO_USB_NAME_DEVICE_FIELD].toString())
+    }
+    if (this[PRINTER_INFO_ADDRESS_FIELD] != null) {
+        return PrinterConnectionInfo.Bluetooth(
+            this[PRINTER_INFO_NAME_DEVICE_FIELD].toString(),
+            this[PRINTER_INFO_ADDRESS_FIELD].toString(),
+        )
+    }
+    return PrinterConnectionInfo.Tcp(
+        this[PRINTER_INFO_ADDRESS_FIELD].toString(),
+        this[PRINTER_INFO_PORT_FIELD] as Int,
+    )
+}
--- core/data/src/main/java/com/casecode/pos/core/data/repository/PrinterRepositoryImpl.kt
+++ core/data/src/main/java/com/casecode/pos/core/data/repository/PrinterRepositoryImpl.kt
@@ -1,17 +1,33 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.core.data.repository
 
 import com.casecode.pos.core.common.AppDispatchers.IO
 import com.casecode.pos.core.common.Dispatcher
 import com.casecode.pos.core.data.R
 import com.casecode.pos.core.data.model.asExternalMapper
-import com.casecode.pos.core.data.service.AuthService
-import com.casecode.pos.core.data.service.checkUserNotFound
-import com.casecode.pos.core.data.utils.PRINTER_INFO_COLLECTION_PATH
-import com.casecode.pos.core.data.utils.getCollectionRefFromUser
+import com.casecode.pos.core.data.model.asExternalModel
+import com.casecode.pos.core.data.utils.checkUserNotFoundAndReturnErrorMessage
+import com.casecode.pos.core.domain.repository.AuthRepository
 import com.casecode.pos.core.domain.repository.PrinterRepository
 import com.casecode.pos.core.domain.utils.Resource
+import com.casecode.pos.core.firebase.services.FirestoreService
+import com.casecode.pos.core.firebase.services.PRINTER_INFO_COLLECTION_PATH
+import com.casecode.pos.core.firebase.services.USERS_COLLECTION_PATH
 import com.casecode.pos.core.model.data.PrinterInfo
-import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import timber.log.Timber
@@ -21,41 +37,43 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 class PrinterRepositoryImpl
-    @Inject
-    constructor(
-        private val firestore: FirebaseFirestore,
-        private val auth: AuthService,
-        @Dispatcher(IO) val ioDispatcher: CoroutineDispatcher,
-    ) : PrinterRepository {
-        override suspend fun getPrinters(): Resource<List<PrinterInfo>> {
-            return withContext(ioDispatcher) {
-                try {
-                    val uid = auth.currentUserId()
-                    auth.checkUserNotFound<List<PrinterInfo>> {
-                        return@withContext it
-                    }
-                    suspendCoroutine { continuation ->
-                        firestore
-                            .getCollectionRefFromUser(uid, PRINTER_INFO_COLLECTION_PATH)
-                            .get()
-                            .addOnSuccessListener { documents ->
-                                val printerMutableList = mutableListOf<PrinterInfo>()
-                                documents.documents.mapNotNull { document ->
-                                    document
-                                        .toObject(PrinterInfo::class.java)
-                                        ?.let { printerMutableList.add(it) }
-                                }
-                                if (printerMutableList.isEmpty()) {
-                                    continuation.resume(Resource.empty())
-                                } else {
-                                    continuation.resume(Resource.success(printerMutableList))
-                                }
-                            }.addOnFailureListener {
-                                Timber.e("${it.message}")
-                                continuation.resume(Resource.error(R.string.core_data_get_printer_info_failure))
+@Inject
+constructor(
+    private val db: FirestoreService,
+    private val auth: AuthRepository,
+    @Dispatcher(IO) val ioDispatcher: CoroutineDispatcher,
+) : PrinterRepository {
+    override suspend fun getPrinters(): Resource<List<PrinterInfo>> {
+        return withContext(ioDispatcher) {
+            try {
+                val uid = auth.currentUserId()
+                auth.checkUserNotFoundAndReturnErrorMessage<List<PrinterInfo>> {
+                    return@withContext it
+                }
+                suspendCoroutine { continuation ->
+                    db.getCollectionChild(USERS_COLLECTION_PATH, uid, PRINTER_INFO_COLLECTION_PATH)
+                        .get()
+                        .addOnSuccessListener { documents ->
+                            val printerMutableList = mutableListOf<PrinterInfo>()
+                            documents.mapNotNull {
                             }
-                    }
-                } catch (e: UnknownHostException) {
+                            documents.documents.mapNotNull { document ->
+
+                                document.asExternalModel()
+                                    .let { printerMutableList.add(it) }
+                            }
+
+                            if (printerMutableList.isEmpty()) {
+                                continuation.resume(Resource.empty())
+                            } else {
+                                continuation.resume(Resource.success(printerMutableList))
+                            }
+                        }.addOnFailureListener {
+                            Timber.e("${it.message}")
+                            continuation.resume(Resource.error(R.string.core_data_get_printer_info_failure))
+                        }
+                }
+            } catch (_: UnknownHostException) {
                 Resource.error(R.string.core_data_get_printer_info_failure_network)
             }
         }
@@ -65,13 +83,11 @@ class PrinterRepositoryImpl
         return withContext(ioDispatcher) {
             try {
                 val uid = auth.currentUserId()
-                auth.checkUserNotFound<Int> {
+                auth.checkUserNotFoundAndReturnErrorMessage<Int> {
                     return@withContext it
                 }
                 suspendCoroutine { continuation ->
-
-                    firestore
-                        .getCollectionRefFromUser(uid, PRINTER_INFO_COLLECTION_PATH)
+                    db.getCollectionChild(USERS_COLLECTION_PATH, uid, PRINTER_INFO_COLLECTION_PATH)
                         .document(printerInfo.name)
                         .set(printerInfo.asExternalMapper())
                         .addOnSuccessListener {
@@ -81,7 +97,7 @@ class PrinterRepositoryImpl
                             continuation.resume(Resource.error(R.string.core_data_add_printer_info_failure_network))
                         }
                 }
-            } catch (e: UnknownHostException) {
+            } catch (_: UnknownHostException) {
                 return@withContext Resource.error(R.string.core_data_add_printer_info_failure_network)
             } catch (e: Exception) {
                 Timber.e("${e.message}")
