bb12dc2ce328ead492d7d73579cb03a1f988a133
Mahmoud AbdAlhafeez <mahmoudabdalhafeez98@gmail.com>
2024-10-27 17:48:34 +0300

    test: add license to ReportsViewModelTest
    This commit adds a license header to the `ReportsViewModelTest` file in the `feature/reports` module.
    Additionally, it updates the test data used in `fetchInvoices_whenHasInvoices_shouldReturnInvoices` to use `invoicesTestData`.

--- feature/reports/src/test/java/com/casecode/pos/feature/statistics/ReportsViewModelTest.kt
+++ feature/reports/src/test/java/com/casecode/pos/feature/statistics/ReportsViewModelTest.kt
@@ -1,7 +1,23 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.feature.statistics
 
 import com.casecode.pos.core.data.R
 import com.casecode.pos.core.testing.base.BaseTest
+import com.casecode.pos.core.testing.data.invoicesTestData
 import com.casecode.pos.core.testing.util.MainDispatcherRule
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -26,7 +42,7 @@ class ReportsViewModelTest : BaseTest() {
     fun fetchInvoices_whenHasInvoices_shouldReturnInvoices() =
         runTest {
             val collectJob = launch(UnconfinedTestDispatcher()) { viewModel.uiState.collect() }
-            val expected = invoiceRepository.fakeInvoices
+            val expected = invoicesTestData
             viewModel.fetchInvoices()
 
             val actual = viewModel.uiState.value.invoices
