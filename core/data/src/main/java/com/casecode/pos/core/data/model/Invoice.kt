d10572a5a65cf918feee3faafddc36db9c5ca6f9
Mahmoud AbdAlhafeez <mahmoudabdalhafeez98@gmail.com>
2024-10-27 17:25:46 +0300

    style: update invoice mapper to use InvoiceDataModel
    This commit updates the `Invoice.asExternalMapper` function to utilize the `InvoiceDataModel` for representing invoice data in the network layer.
    
    Instead of directly mapping `Invoice` properties to a `Map`, it now creates an instance of `InvoiceDataModel` and then constructs the `Map` based on the data model's fields.
    
    This change improves the consistency and structure of invoice data representation when interacting with the network, aligning with the defined `InvoiceDataModel` structure.
    
    Additionally, the constants for invoice fields have been updated to use those defined in the `InvoiceDataModel` class, further enhancing data consistency.

--- core/data/src/main/java/com/casecode/pos/core/data/model/Invoice.kt
+++ core/data/src/main/java/com/casecode/pos/core/data/model/Invoice.kt
@@ -1,40 +1,44 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.core.data.model
 
-import com.casecode.pos.core.data.utils.Invoice_CREATEDBY_FIELD
-import com.casecode.pos.core.data.utils.Invoice_CUSTOMER_FIELD
-import com.casecode.pos.core.data.utils.Invoice_DATE_FIELD
-import com.casecode.pos.core.data.utils.Invoice_ITEMS_FIELD
-import com.casecode.pos.core.data.utils.Invoice_NAME_FIELD
 import com.casecode.pos.core.data.utils.toDateFormatString
-import com.casecode.pos.core.model.data.users.Customer
+import com.casecode.pos.core.firebase.services.INVOICE_CREATED_BY_FIELD
+import com.casecode.pos.core.firebase.services.INVOICE_CUSTOMER_FIELD
+import com.casecode.pos.core.firebase.services.INVOICE_DATE_FIELD
+import com.casecode.pos.core.firebase.services.INVOICE_ITEMS_FIELD
+import com.casecode.pos.core.firebase.services.INVOICE_NAME_FIELD
+import com.casecode.pos.core.firebase.services.model.InvoiceDataModel
 import com.casecode.pos.core.model.data.users.Invoice
 import com.casecode.pos.core.model.data.users.InvoiceGroup
-import com.casecode.pos.core.model.data.users.Item
-import com.google.firebase.firestore.DocumentId
 import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.ServerTimestamp
-import java.util.Date
-
-data class InvoiceDataModel(
-    @DocumentId val invoiceId: String = "",
-    @ServerTimestamp val date: Date = Date(),
-    val createdBy: String = "",
-    val customer: Customer? = null,
-    val items: List<Item> = emptyList(),
-)
 
 fun Invoice.asExternalMapper(
     documentRef: DocumentReference,
     createdBy: String,
 ): Map<String, Any?> {
-    val networkInvoice = InvoiceDataModel(documentRef.id, this.date, createdBy, this.customer, this.items)
+    val networkInvoice =
+        InvoiceDataModel(documentRef.id, this.date, createdBy, this.customer, this.items)
     val invoiceMap =
         mapOf(
-            Invoice_NAME_FIELD to documentRef.id,
-            Invoice_DATE_FIELD to networkInvoice.date,
-            Invoice_CREATEDBY_FIELD to networkInvoice.createdBy,
-            Invoice_CUSTOMER_FIELD to networkInvoice.customer,
-            Invoice_ITEMS_FIELD to networkInvoice.items,
+            INVOICE_NAME_FIELD to documentRef.id,
+            INVOICE_DATE_FIELD to networkInvoice.date,
+            INVOICE_CREATED_BY_FIELD to networkInvoice.createdBy,
+            INVOICE_CUSTOMER_FIELD to networkInvoice.customer,
+            INVOICE_ITEMS_FIELD to networkInvoice.items,
         )
     return invoiceMap
 }
