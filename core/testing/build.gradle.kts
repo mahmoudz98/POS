3f78d7718b1d76c639006ea62f1dc51cbf7af15d
Mahmoud AbdAlhafeez <mahmoudabdalhafeez98@gmail.com>
2024-10-27 17:38:07 +0300

    style: add license headers and update dependencies in core/testing module
    This commit adds license information to the `build.gradle.kts` file in the core/testing module.
    It also modifies the dependencies:
    
    - Adds `core.firebaseServices` as an API dependency.
    - Removes dependency on Hamcrest library.
    - Replaces `mockk.agent` with  `mockk.android` implementation dependency.
    - Removes the Firebase library plugin.

--- core/testing/build.gradle.kts
+++ core/testing/build.gradle.kts
@@ -1,8 +1,40 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 plugins {
     alias(libs.plugins.pos.android.library)
     alias(libs.plugins.pos.hilt)
-    alias(libs.plugins.pos.android.firebase.library)
+    // alias(libs.plugins.pos.android.firebase.library)
 }
 
 android {
@@ -12,16 +44,16 @@ android {
 dependencies {
 
     api(projects.core.data)
+    api(projects.core.firebaseServices)
 
-    // api(libs.test.hamcrest)
-    //  compileOnly(libs.test.hamcrest.library)
     implementation(libs.googleid)
 
     api(libs.coroutines.test)
+
     api(libs.mockk.android) {
         exclude(group = "org.junit.jupiter", module = "junit-jupiter")
     }
-    implementation(libs.mockk.agent)
+    //implementation(libs.mockk.agent)
     implementation(libs.androidx.test.runner)
     implementation(libs.androidx.test.rules)
     implementation(libs.hilt.android.testing)
