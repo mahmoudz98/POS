6b1dac48e820bbb2f15cfc2aaa4fb3440cfc77d0
Mahmoud Abdalhafeez <mah99239@github.com>
2024-10-27 17:35:27 +0300

    feat(model): add Supplier, CashUp classes and enhance existing models
    This commit introduces the `Supplier` and `CashUp` data classes to the model.
    It also enhances the existing `Employee`, `Invoice` and `Item` data classes with new functionalities and properties:
    
    - Adds a `isEmployeeNameDuplicate` method to `Employee` to check for duplicate employee names.
    - Updates `Invoice` to calculate the total using unit prices and item quantities and adjusts item addition logic.
    - Adds a `toStoreType` method to `StoreType` for retrieving StoreType based on name.
    - Modifies `Item` properties `costPrice` and `unitPrice` to Double type, and adds new methods to check for stock levels and overrides `equals` and `hashCode`.

--- core/printer/build.gradle.kts
+++ core/printer/build.gradle.kts
@@ -1,6 +1,22 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 plugins {
     alias(libs.plugins.pos.android.library)
     alias(libs.plugins.pos.hilt)
+    alias(libs.plugins.pos.android.library.jacoco)
 }
 
 android {
@@ -9,7 +25,7 @@ android {
 
 dependencies {
     implementation(projects.core.model)
-    implementation(projects.core.data)
+    implementation(projects.core.firebaseServices)
     implementation(projects.core.common)
     api(libs.escpos.thermalprinter.android)
 
