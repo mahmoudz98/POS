4fa7aa92f9acd721b228af45964cb6ffc2cd479e
Mahmoud AbdAlhafeez <mahmoudabdalhafeez98@gmail.com>
2024-10-27 17:28:35 +0300

    style: add license headers to core/data/di/RepositoryModule.kt
    This commit adds license headers to `RepositoryModule.kt` and includes bindings for AccountRepository and AuthRepository.
    
    The license information includes the copyright holder, year, and the MIT License terms.
    
    Additionally, the following changes are made:
    
    - Binds `AccountRepositoryImpl` to `AccountRepository`.
    - Binds `AuthRepositoryImpl` to `AuthRepository`.
    - Adds `@Singleton` annotation to `ItemRepository` binding.

--- core/data/src/main/java/com/casecode/pos/core/data/di/RepositoryModule.kt
+++ core/data/src/main/java/com/casecode/pos/core/data/di/RepositoryModule.kt
@@ -1,5 +1,22 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.core.data.di
 
+import com.casecode.pos.core.data.repository.AccountRepositoryImpl
+import com.casecode.pos.core.data.repository.AuthRepositoryImpl
 import com.casecode.pos.core.data.repository.BusinessRepositoryImpl
 import com.casecode.pos.core.data.repository.EmployeesBusinessRepositoryImpl
 import com.casecode.pos.core.data.repository.InvoiceRepositoryImpl
@@ -8,6 +25,8 @@ import com.casecode.pos.core.data.repository.ItemRepositoryImpl
 import com.casecode.pos.core.data.repository.PrinterRepositoryImpl
 import com.casecode.pos.core.data.repository.SubscriptionsBusinessRepositoryImpl
 import com.casecode.pos.core.data.repository.SubscriptionsRepositoryImpl
+import com.casecode.pos.core.domain.repository.AccountRepository
+import com.casecode.pos.core.domain.repository.AuthRepository
 import com.casecode.pos.core.domain.repository.BusinessRepository
 import com.casecode.pos.core.domain.repository.EmployeesBusinessRepository
 import com.casecode.pos.core.domain.repository.InvoiceRepository
@@ -20,10 +39,17 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+    @Binds
+    internal abstract fun bindAccountRepository(accountServiceImpl: AccountRepositoryImpl): AccountRepository
+
+    @Binds
+    internal abstract fun bindAuthRepository(authImpl: AuthRepositoryImpl): AuthRepository
+
     @Binds
     internal abstract fun bindBusinessRepo(businessRepositoryImpl: BusinessRepositoryImpl): BusinessRepository
 
@@ -40,6 +66,7 @@ abstract class RepositoryModule {
     @Binds
     internal abstract fun bindSubscriptionsRepo(subscriptionsRepositoryImpl: SubscriptionsRepositoryImpl): SubscriptionsRepository
 
+    @Singleton
     @Binds
     internal abstract fun bindItemRepo(itemRepositoryImpl: ItemRepositoryImpl): ItemRepository
 
