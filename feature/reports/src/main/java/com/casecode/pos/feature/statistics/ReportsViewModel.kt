d9c7344e5f19298b9edda472e8d13f13110ac271
Mahmoud AbdAlhafeez <mahmoudabdalhafeez98@gmail.com>
2024-10-27 17:48:46 +0300

    style: add license headers to reports module
    This commit adds license headers to the following files in the `feature/reports` module:
    
    - `UiReportsState.kt`
    - `values-ar/strings.xml`
    - `ReportsViewModel.kt`
    - `ReportsScreen.kt`
    - `ReportsNavigation.kt`
    - `AndroidManifest.xml`
    - `values/strings.xml`

--- feature/reports/src/main/java/com/casecode/pos/feature/statistics/ReportsViewModel.kt
+++ feature/reports/src/main/java/com/casecode/pos/feature/statistics/ReportsViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.feature.statistics
 
 import androidx.lifecycle.ViewModel
@@ -14,13 +29,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ReportsViewModel
-    @Inject
-    constructor(
-        private val networkMonitor: NetworkMonitor,
-        private val getTodayInvoicesUseCase: GetTodayInvoicesUseCase,
-    ) : ViewModel() {
-        private val _uiState = MutableStateFlow(UiReportsState())
-        val uiState = _uiState.asStateFlow()
+@Inject
+constructor(
+    private val networkMonitor: NetworkMonitor,
+    private val getTodayInvoicesUseCase: GetTodayInvoicesUseCase,
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(UiReportsState())
+    val uiState = _uiState.asStateFlow()
 
     fun fetchInvoices() {
         viewModelScope.launch {
