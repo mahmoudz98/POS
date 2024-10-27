da1fd7b73fc3516988d30c79811ec34fc7bbdd60
Mahmoud AbdAlhafeez <mahmoudabdalhafeez98@gmail.com>
2024-10-27 03:58:10 +0300

    feat(ui): add stateInWhileSubscribed and update Item model

--- core/ui/src/main/java/com/casecode/pos/core/ui/ItemsPreviewParameterProvider.kt
+++ core/ui/src/main/java/com/casecode/pos/core/ui/ItemsPreviewParameterProvider.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.core.ui
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
@@ -14,22 +29,22 @@ class ItemsPreviewParameterProvider : PreviewParameterProvider<List<Item>> {
                 listOf(
                     Item(
                         name = "Iphone13",
-                        price = 200.0,
-                        quantity = 1.0,
+                        unitPrice = 200.0,
+                        quantity = 1,
                         imageUrl = "",
                         sku = "12345",
                     ),
                     Item(
                         name = "IPhone14",
-                        price = 2000.0,
-                        quantity = 112.0,
+                        unitPrice = 2000.0,
+                        quantity = 11,
                         imageUrl = "",
                         sku = "12342256",
                     ),
                     Item(
                         name = "IPhone15",
-                        price = 3000.0,
-                        quantity = 1.0,
+                        unitPrice = 3000.0,
+                        quantity = 1,
                         imageUrl = "",
                         sku = "1232456",
                     ),
---
+++ core/ui/src/main/java/com/casecode/pos/core/ui/StateInWhileSubscribed.kt
@@ -0,0 +1,31 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.casecode.pos.core.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+
+context(ViewModel)
+@Suppress("CONTEXT_RECEIVERS_DEPRECATED")
+fun <T> Flow<T>.stateInWhileSubscribed(initialValue: T): StateFlow<T> = stateIn(
+    scope = viewModelScope,
+    started = SharingStarted.WhileSubscribed(5_000),
+    initialValue = initialValue,
+)
