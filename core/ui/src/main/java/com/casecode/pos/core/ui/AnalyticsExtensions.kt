f68b05e2fcae18072b73e0814396c7360027374f
Mahmoud AbdAlhafeez <mahmoudabdalhafeez98@gmail.com>
2024-10-30 19:26:55 +0300

    feat(core/ui): add analytics and jank tracking extensions
    This commit introduces new extension functions for analytics and jank tracking in the core/ui module.
    
    **AnalyticsExtensions.kt**
    
    - Adds functions for logging screen views and news resource openings.
    - Provides a composable function `TrackScreenViewEvent` to automatically record screen views.
    
    **JankStatsExtensions.kt**
    
    - Adds functions for tracking jank using PerformanceMetricsState.
    - Includes `TrackJank` and `TrackDisposableJank` for reporting jank metrics.
    - Introduces `TrackScrollJank` to monitor jank during scrolling.
    
    These extensions streamline analytics logging and performance monitoring within the UI layer.

---
+++ core/ui/src/main/java/com/casecode/pos/core/ui/AnalyticsExtensions.kt
@@ -0,0 +1,62 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.casecode.pos.core.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import com.casecode.pos.core.analytics.AnalyticsEvent
+import com.casecode.pos.core.analytics.AnalyticsEvent.Param
+import com.casecode.pos.core.analytics.AnalyticsEvent.ParamKeys
+import com.casecode.pos.core.analytics.AnalyticsEvent.Types
+import com.casecode.pos.core.analytics.AnalyticsHelper
+import com.casecode.pos.core.analytics.LocalAnalyticsHelper
+
+/**
+ * Classes and functions associated with analytics events for the UI.
+ */
+fun AnalyticsHelper.logScreenView(screenName: String) {
+    logEvent(
+        AnalyticsEvent(
+            type = Types.SCREEN_VIEW,
+            extras = listOf(
+                Param(ParamKeys.SCREEN_NAME, screenName),
+            ),
+        ),
+    )
+}
+
+fun AnalyticsHelper.logNewsResourceOpened(newsResourceId: String) {
+    logEvent(
+        event = AnalyticsEvent(
+            type = "news_resource_opened",
+            extras = listOf(
+                Param("opened_news_resource", newsResourceId),
+            ),
+        ),
+    )
+}
+
+/**
+ * A side-effect which records a screen view event.
+ */
+@Composable
+fun TrackScreenViewEvent(
+    screenName: String,
+    analyticsHelper: AnalyticsHelper = LocalAnalyticsHelper.current,
+) = DisposableEffect(Unit) {
+    analyticsHelper.logScreenView(screenName)
+    onDispose {}
+}
