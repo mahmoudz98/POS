6fc97888da8790960c5ea15fee2b844794c837da
Mahmoud AbdAlhafeez <mahmoudabdalhafeez98@gmail.com>
2024-10-30 19:26:17 +0300

    feat(core): add analytics module
    This commit introduces a new analytics module, which provides the capability to log events using Firebase Analytics.
    
    The following changes were made:
    
    - Added `AnalyticsEvent` class to represent analytics events and associated parameters.
    - Added `AnalyticsHelper` interface for logging events, with implementations for Firebase (production), Stub (demo), and NoOp (tests and previews).
    - Created `LocalAnalyticsHelper` composition local to provide access to the `AnalyticsHelper` instance.
    - Created modules for production, demo, and test builds, installing them into the appropriate scopes.
    - Added license headers to all new files.
    - Added build configuration for the new module, including dependencies for Firebase Analytics.
    - Implemented truncation logic for analytics event parameters to adhere to Firebase limitations.
    
    This change allows for consistent logging of analytics events across the application, enabling better user behavior analysis and app improvements.

---
+++ core/analytics/src/main/kotlin/com/casecode/pos/core/analytics/StubAnalyticsHelper.kt
@@ -0,0 +1,33 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.casecode.pos.core.analytics
+
+import android.util.Log
+import javax.inject.Inject
+import javax.inject.Singleton
+
+private const val TAG = "StubAnalyticsHelper"
+
+/**
+ * An implementation of AnalyticsHelper just writes the events to logcat. Used in builds where no
+ * analytics events should be sent to a backend.
+ */
+@Singleton
+internal class StubAnalyticsHelper @Inject constructor() : AnalyticsHelper {
+    override fun logEvent(event: AnalyticsEvent) {
+        Log.d(TAG, "Received analytics event: $event")
+    }
+}
