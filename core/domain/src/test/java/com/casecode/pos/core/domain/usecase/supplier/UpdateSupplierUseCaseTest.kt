fcb79ba4e3e7b69323a81505819df6d251622acf
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2024-11-11 20:02:06 +0200

    test(supplier): add unit tests for supplier use cases
    Added unit tests for the following supplier use cases:
    
    - `AddSupplierUseCase`
    - `DeleteSupplierUseCase`
    - `UpdateSupplierUseCase`
    - `GetSuppliersUseCase`
    
    These tests cover various scenarios, including successful operations, error handling, and empty results.

---
+++ core/domain/src/test/java/com/casecode/pos/core/domain/usecase/supplier/UpdateSupplierUseCaseTest.kt
@@ -0,0 +1,71 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.casecode.pos.core.domain.usecase.supplier
+
+import com.casecode.pos.core.domain.usecase.UpdateSupplierUseCase
+import com.casecode.pos.core.domain.utils.OperationResult
+import com.casecode.pos.core.model.data.users.Supplier
+import com.casecode.pos.core.testing.repository.TestSupplierRepository
+import com.casecode.pos.core.testing.util.MainDispatcherRule
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import com.casecode.pos.core.data.R.string as stringData
+
+class UpdateSupplierUseCaseTest {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    // Subject under test
+    private val testSupplierRepository = TestSupplierRepository()
+    private val updateSupplierUseCase = UpdateSupplierUseCase(testSupplierRepository)
+    private val oldSupplier = Supplier(
+        id = "1",
+        companyName = "Samsung",
+        contactName = "John Doe",
+        contactPhone = "12345678902131",
+        contactEmail = "testold@supplier.com",
+        address = "Test Address",
+        category = "Electronics",
+    )
+
+    private val updateSupplier = Supplier(
+        id = "1",
+        companyName = "Samsung",
+        contactName = "John Doe",
+        contactPhone = "1234567890",
+        contactEmail = "test@supplier.com",
+        address = "Test Address",
+        category = "Electronics",
+    )
+
+    @Test
+    fun whenSupplierUpdated_returnSuccess() = runTest {
+        val result = updateSupplierUseCase(oldSupplier, updateSupplier)
+        assertEquals(OperationResult.Success, result)
+    }
+
+    @Test
+    fun whenHasError_returnsError() = runTest {
+        testSupplierRepository.setReturnError(true)
+        val result = updateSupplierUseCase(oldSupplier, updateSupplier)
+        assertEquals(
+            OperationResult.Failure(stringData.core_data_update_supplier_failure_generic),
+            result,
+        )
+    }
+}
