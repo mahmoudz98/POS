79416c16a81831b8dd27e0672049ce42ceb71d06
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2024-11-15 00:02:00 +0200

    feat(model): add SupplierInvoice data class
    Added a new data class `SupplierInvoice` to represent supplier invoices.
    
    This class includes information about the invoice such as:
    - invoiceId
    - supplierId
    - createdAt
    - createdBy
    - totalAmount
    - paymentStatus
    - invoiceItems

---
+++ core/model/src/main/java/com/casecode/pos/core/model/data/users/SupplierInvoice.kt
@@ -0,0 +1,36 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.casecode.pos.core.model.data.users
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+
+data class SupplierInvoice(
+    val invoiceId: String = "",
+    val supplierId: String = "",
+    val createdAt: Instant = Clock.System.now(),
+    val createdBy: String = "",
+    val totalAmount: Double = 0.0,
+    val paymentStatus: PaymentStatus = PaymentStatus.PENDING,
+    val invoiceItems: List<Item> = emptyList(),
+)
+
+enum class PaymentStatus {
+    PENDING,
+    PAID,
+    PARTIALLY_PAID,
+    OVERDUE,
+}
