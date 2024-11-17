7542e82c6410d494502987da108afe78571a326d
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2024-11-17 23:34:17 +0200

    feat(purchase): add purchase feature
    Added a new purchase feature that allows users to manage bills and suppliers.
    
    - Created a Purchase screen with buttons for navigating to Bills and Suppliers screens.
    - Added navigation logic for the purchase feature.
    - Defined string resources for the purchase feature.
    - Added dependencies for the purchase feature.

---
+++ feature/purchase/src/main/kotlin/com/casecode/pos/feature/purchase/navigation/PurchaseNavigation.kt
@@ -0,0 +1,40 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.casecode.pos.feature.purchase.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.casecode.pos.feature.purchase.PurchaseScreen
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object PurchaseRoute
+
+fun NavGraphBuilder.purchaseScreen(
+    onBillsScreenClick: () -> Unit,
+    onSupplierScreenClick: () -> Unit,
+) {
+    composable<PurchaseRoute> {
+        PurchaseScreen(
+            onBillsScreenClick = onBillsScreenClick,
+            onSupplierScreenClick = onSupplierScreenClick,
+        )
+    }
+}
+
+fun NavController.navigateToPurchase(navOptions: NavOptions) = navigate(PurchaseRoute, navOptions)
