b9446fd05de63ae4dec83595515d486e82af71c8
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2024-11-30 22:10:28 +0200

    feat(core-ui): add Double extension functions for formatting
    Added two extension functions for `Double` to handle formatting:
    
    - `toFormattedString()`: Converts a nullable `Double` to a formatted `String`, removing trailing zeros. Returns an empty string if the `Double` is null.
    - `toBigDecimalFormatted()`: Formats a `Double` to a `String` with a specified number of decimal places (defaults to 2). Removes trailing zeros.

--- core/ui/src/main/java/com/casecode/pos/core/ui/utils/String.kt
+++ core/ui/src/main/java/com/casecode/pos/core/ui/utils/String.kt
@@ -15,5 +15,28 @@
  */
 package com.casecode.pos.core.ui.utils
 
+/**
+ * Converts a nullable Double to a formatted String.
+ *
+ * This function converts the Double to a BigDecimal, strips any trailing zeros, and then converts it to a plain String.
+ * If the Double is null, an empty String is returned.
+ *
+ * @return A formatted String representation of the Double, or an empty String if the Double is null.
+ */
 fun Double?.toFormattedString(): String =
     this?.toBigDecimal()?.stripTrailingZeros()?.toPlainString() ?: ""
+
+/**
+ * Formats a Double to a String with a specified number of decimal places.
+ *
+ * This function converts the Double to a BigDecimal, scales it to the desired number of decimal places,
+ * removes trailing zeros, and returns the result as a plain String.
+ *
+ * @param decimalPlaces The number of decimal places to include in the formatted string. Defaults to 2.
+ * @return The formatted string representation of the Double.
+ */
+fun Double.toBigDecimalFormatted(decimalPlaces: Int = 2): String =
+    toBigDecimal()
+        .setScale(decimalPlaces, java.math.RoundingMode.HALF_UP)
+        .stripTrailingZeros()
+        .toPlainString()
