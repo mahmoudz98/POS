bfc30bdfb43eddf016275f23d76342d38f7c9e29
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2024-12-22 22:08:32 +0200

    feat(bill): add UI tests for bill creation
    Added UI tests for the bill creation feature.
    
    - Created `AddOrUpdateBillItemScreenTest` and `AddOrUpdateBillScreenTest` for testing bill creation UI.
    - Added tests to verify error messages when bill input is empty or partially filled.
    - Refactored the tests to use a common save button label string.

---
+++ feature/bill/src/androidTest/kotlin/com/casecode/pos/feature/bill/detials/AddOrUpdateBillScreenTest.kt
@@ -0,0 +1,115 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.casecode.pos.feature.bill.detials
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.casecode.pos.feature.bill.R
+import com.casecode.pos.feature.bill.creation.AddBillScreen
+import com.casecode.pos.feature.bill.creation.BillInputState
+import com.casecode.pos.feature.bill.creation.SearchSupplierUiState
+import org.junit.Rule
+import org.junit.Test
+import com.casecode.pos.core.ui.R.string as uiString
+
+class AddOrUpdateBillScreenTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    private val saveButtonLabel by lazy {
+        composeTestRule.activity.getString(uiString.core_ui_save_action_text)
+    }
+
+    @Test
+    fun whenBillInputEmpty_assertErrorsIsDisplayed() {
+        composeTestRule.setContent {
+            AddBillScreen(
+                billInputState = BillInputState(),
+                searchSupplier = "",
+                userMessage = null,
+                filterSupplierState = SearchSupplierUiState.EmptyQuery,
+                onSearchSupplierChange = {},
+                onNavigateBack = {},
+                onAddBillItem = {},
+                onUpdateBillItem = {},
+                onRemoveBillItem = {},
+                showSnackbarMessage = {},
+                onShownMessage = {},
+                onAddBill = {},
+
+            )
+        }
+        composeTestRule.onNodeWithText(saveButtonLabel).performClick()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.feature_bill_supplier_not_selected_message,
+            ),
+        ).assertIsDisplayed()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.feature_bill_number_empty,
+            ),
+        ).assertIsDisplayed()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.feature_bill_items_empty_message,
+            ),
+        )
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun whenHasBillInput_assertErrorsIsDisplayed() {
+        composeTestRule.setContent {
+            AddBillScreen(
+                billInputState = BillInputState(),
+                searchSupplier = "",
+                userMessage = null,
+                filterSupplierState = SearchSupplierUiState.EmptyQuery,
+                onSearchSupplierChange = {},
+                onNavigateBack = {},
+                onAddBillItem = {},
+                onUpdateBillItem = {},
+                onRemoveBillItem = {},
+                showSnackbarMessage = {},
+                onShownMessage = {},
+                onAddBill = {},
+
+            )
+        }
+        composeTestRule.onNodeWithText(saveButtonLabel).performClick()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.feature_bill_supplier_empty,
+            ),
+        )
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.feature_bill_number_empty,
+            ),
+        )
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.feature_bill_items_empty_message,
+            ),
+        )
+            .assertIsDisplayed()
+    }
+}
