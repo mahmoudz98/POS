6d101e2c221f12b93d6f7c35b2bb27756e4394e2
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2024-12-22 16:14:05 +0200

    feat(domain): add copyright and update item stock use case test
    - Added a copyright header to `GetSupplierInvoiceDetailsUseCaseTest.kt`.
    - Updated `UpdateStockInItemsUseCaseTest.kt` to use `OperationResult` instead of `Resource` and updated assertions to match.

--- core/domain/src/test/java/com/casecode/pos/core/domain/usecase/supplierinvoice/GetSupplierInvoiceDetailsUseCaseTest.kt
+++ core/domain/src/test/java/com/casecode/pos/core/domain/usecase/supplierinvoice/GetSupplierInvoiceDetailsUseCaseTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.core.domain.usecase.supplierinvoice
 
 import com.casecode.pos.core.domain.usecase.GetSupplierInvoiceDetailsUseCase
@@ -5,7 +20,6 @@ import com.casecode.pos.core.domain.utils.Resource
 import com.casecode.pos.core.testing.repository.TestSupplierInvoicesRepository
 import com.casecode.pos.core.testing.util.MainDispatcherRule
 import kotlinx.coroutines.flow.last
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
@@ -24,7 +38,7 @@ class GetSupplierInvoiceDetailsUseCaseTest {
     @Test
     fun `whenHasSupplierId_returnsSupplierInvoice`() = runTest {
         val supplierInvoiceResult = getSupplierInvoiceDetailsUseCase("INV001")
-        backgroundScope.launch(UnconfinedTestDispatcher()){supplierInvoiceResult.collect{}}
+        backgroundScope.launch(UnconfinedTestDispatcher()) { supplierInvoiceResult.collect {} }
         assertEquals(
             Resource.Success(testRepo.supplierInvoicesTest[0]),
             supplierInvoiceResult.last(),
