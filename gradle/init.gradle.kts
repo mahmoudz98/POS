c60747ff4d078b910593edc621afae208a6b7f9a
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2024-12-22 22:00:51 +0200

    feat(build): update gradle and lint configuration
    Updated gradle configuration and added lint support.
    
    - Updated ktlint version to 1.4.0 and spotless to 6.25.0.
    - Added android-lint plugin and lint-gradle dependency to `libs.versions.toml`.
    - Applied spotless plugin to kts files.
    - Updated ktlint configuration in `init.gradle.kts` to include android and license headers.

--- gradle/init.gradle.kts
+++ gradle/init.gradle.kts
@@ -1,7 +1,22 @@
-val ktlintVersion = "1.0.1"
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+val ktlintVersion = "1.4.0"
 
 initscript {
-    val spotlessVersion = "6.23.3"
+    val spotlessVersion = "6.25.0"
 
     repositories {
         mavenCentral()
@@ -13,14 +28,17 @@ initscript {
 }
 
 rootProject {
-
     subprojects {
         apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
         extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
             kotlin {
                 target("**/*.kt")
                 targetExclude("**/build/**/*.kt")
-                ktlint(ktlintVersion).editorConfigOverride(mapOf("android" to "true"))
+                ktlint(ktlintVersion).editorConfigOverride(
+                    mapOf(
+                        "android" to "true",
+                    ),
+                )
                 licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
             }
             format("kts") {
