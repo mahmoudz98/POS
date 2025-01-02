888ee4c0fb0a55956951c77e5ac7e8d838911197
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2025-01-02 17:21:57 +0200

    feat(designsystem): add Text and Border composables
    Added `Text` and `Border` composables to the design system.
    
    - Created `FilterTitleText` composable for displaying styled title text.
    - Added `diagonalGradientBorder` modifier for creating borders with a diagonal gradient.
    - Added a copyright header to both `Text.kt` and `Border.kt`.

---
+++ core/designsystem/src/main/java/com/casecode/pos/core/designsystem/component/Text.kt
@@ -0,0 +1,33 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.casecode.pos.core.designsystem.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FilterTitleText(modifier: Modifier = Modifier, text: String) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.titleMedium,
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(bottom = 8.dp),
+    )
+}
