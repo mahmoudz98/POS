2eafb7f21bc6bf2ae54da27b2a30255cecc3d64e
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2025-05-26 11:37:15 +0300

    Refactor: Update MenuAnchorType to ExposedDropdownMenuAnchorType
    Signed-off-by: Mahmooud Abdalhafeez <mahmoud99239@gmail.com>

--- core/ui/src/main/java/com/casecode/pos/core/ui/EmployeeDialogContent.kt
+++ core/ui/src/main/java/com/casecode/pos/core/ui/EmployeeDialogContent.kt
@@ -22,9 +22,9 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
