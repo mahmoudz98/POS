c7a7aed8a4b2f859997adf0ff68c0c0e974cd48a
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2025-05-26 11:30:48 +0300

    Refactor: Update MenuAnchorType to ExposedDropdownMenuAnchorType
    Signed-off-by: Mahmooud Abdalhafeez <mahmoud99239@gmail.com>

--- feature/stepper/src/main/java/com/casecode/pos/feature/stepper/BusinessInfoScreen.kt
+++ feature/stepper/src/main/java/com/casecode/pos/feature/stepper/BusinessInfoScreen.kt
@@ -31,10 +31,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowRight
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -111,7 +111,7 @@ internal fun BusinessInfoScreen(
                     readOnly = true,
                     modifier =
                     Modifier
-                        .menuAnchor(MenuAnchorType.PrimaryNotEditable)
+                        .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable)
                         .fillMaxWidth(),
                     label = stringResource(id = uiString.core_ui_store_type_hint),
                     value = storeType,
