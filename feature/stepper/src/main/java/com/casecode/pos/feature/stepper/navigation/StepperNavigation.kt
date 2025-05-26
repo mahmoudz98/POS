4aec5bb1acf1d8e6a2b7b0da6f9c539c2d871d51
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2025-05-26 11:31:04 +0300

    Add license header to StepperNavigation.kt
    The MIT license header has been added to the StepperNavigation.kt file.
    
    Signed-off-by: Mahmooud Abdalhafeez <mahmoud99239@gmail.com>

--- feature/stepper/src/main/java/com/casecode/pos/feature/stepper/navigation/StepperNavigation.kt
+++ feature/stepper/src/main/java/com/casecode/pos/feature/stepper/navigation/StepperNavigation.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed 2024 by Mahmood Abdalhafeez
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.casecode.pos.feature.stepper.navigation
 
 import androidx.navigation.NavController
@@ -7,7 +22,6 @@ import androidx.navigation.compose.composable
 import com.casecode.pos.feature.stepper.StepperScreen
 import kotlinx.serialization.Serializable
 
-
 @Serializable
 data object StepperRoute
 
@@ -15,15 +29,12 @@ fun NavGraphBuilder.stepperScreen(
     onStepperCompleteToHome: () -> Unit,
     onBackToSignIn: () -> Unit,
 ) {
-
     composable<StepperRoute> {
-
         StepperScreen(
             onStepperCompleteToHome = onStepperCompleteToHome,
             onBackToSignIn = onBackToSignIn,
         )
     }
 }
-
 
 fun NavController.navigateToStepper(navOptions: NavOptions) = navigate(StepperRoute, navOptions)
