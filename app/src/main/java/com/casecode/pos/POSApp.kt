81c401c099ab4b79326f451de1f0537dded9ea85
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2025-05-26 10:24:40 +0300

    Enable StrictMode for debug builds
    Signed-off-by: Mahmooud Abdalhafeez <mahmoud99239@gmail.com>

--- app/src/main/java/com/casecode/pos/POSApp.kt
+++ app/src/main/java/com/casecode/pos/POSApp.kt
@@ -16,10 +16,13 @@
 package com.casecode.pos
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
+import android.os.StrictMode
 import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import timber.log.Timber.DebugTree
+import android.os.StrictMode.ThreadPolicy.Builder
 
 @HiltAndroidApp
 class POSApp : Application() {
@@ -27,5 +30,25 @@ class POSApp : Application() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
         Timber.plant(DebugTree())
+        setStrictModePolicy()
+    }
+    /**
+     * Return true if the application is debuggable.
+     */
+    private fun isDebuggable(): Boolean {
+        return 0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+    }
+    /**
+     * Set a thread policy that detects all potential problems on the main thread, such as network
+     * and disk access.
+     *
+     * If a problem is found, the offending call will be logged and the application will be killed.
+     */
+    private fun setStrictModePolicy() {
+        if (isDebuggable()) {
+            StrictMode.setThreadPolicy(
+                Builder().detectAll().penaltyLog().build(),
+            )
+        }
     }
 }
