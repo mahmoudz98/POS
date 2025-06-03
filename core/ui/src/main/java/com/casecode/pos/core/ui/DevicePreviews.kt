1baa2e925ac2d70cd6cf105b08dc906a06e8ce9b
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2025-06-03 15:48:37 +0300

    Add Tablet Landscape and Dark Mode previews to DevicePreviews
    This commit enhances the `@DevicePreviews` annotation to include two new preview configurations:
    
    -   **Tablet - Landscape**: Utilizes the predefined `TABLET` device specification and sets `showSystemUi = true` to better represent the landscape orientation on a tablet.
    -   **Dark**: Enables dark mode for previews by setting `uiMode = UI_MODE_NIGHT_YES or UI_MODE_TYPE_NORMAL`.
    
    Signed-off-by: Mahmooud Abdalhafeez <mahmoud99239@gmail.com>

--- core/ui/src/main/java/com/casecode/pos/core/ui/DevicePreviews.kt
+++ core/ui/src/main/java/com/casecode/pos/core/ui/DevicePreviews.kt
@@ -15,6 +15,8 @@
  */
 package com.casecode.pos.core.ui
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.content.res.Configuration.UI_MODE_TYPE_NORMAL
 import androidx.compose.ui.tooling.preview.Devices.TABLET
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -31,6 +33,9 @@ import androidx.compose.ui.tooling.preview.Preview
 )
 @Preview(name = "foldable", device = "spec:shape=Normal,width=673,height=841,unit=dp,dpi=480")
 @Preview(name = "tablet", device = "spec:shape=Normal,width=1280,height=800,unit=dp,dpi=480")
+@Preview(name = "Tablet - Landscape", device = TABLET, showSystemUi = true)
+@Preview(name = "Dark", uiMode = UI_MODE_NIGHT_YES or UI_MODE_TYPE_NORMAL)
+
 annotation class DevicePreviews
 
 @Preview(
