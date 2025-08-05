7d80d97b9cabdfb595e64b04e1d28b1ccfa1e985
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2025-08-05 18:46:22 +0300

    Docs: Update app version to 0.1.0
    Signed-off-by: Mahmooud Abdalhafeez <mahmoud99239@gmail.com>

--- build-logic/convention/src/main/kotlin/com/casecode/pos/Configuration.kt
+++ build-logic/convention/src/main/kotlin/com/casecode/pos/Configuration.kt
@@ -1,11 +1,11 @@
 package com.casecode.pos
 
 object Configuration {
-    const val COMPILE_SDK = 35
-    const val TARGET_SDK = 35
+    const val COMPILE_SDK = 36
+    const val TARGET_SDK = 36
     const val MIN_SDK = 24
-    const val MAJOR_VERSION = 1
-    const val MINOR_VERSION = 0
+    const val MAJOR_VERSION = 0
+    const val MINOR_VERSION = 1
     const val PATCH_VERSION = 0
     const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
     const val VERSION_CODE = 1
