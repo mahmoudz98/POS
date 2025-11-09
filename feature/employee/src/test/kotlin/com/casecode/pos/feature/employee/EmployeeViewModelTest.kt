753ea4b6135a5811d41c950f3528dfeaf1d64a0c
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2025-11-09 08:11:15 +0200

    fix test to verify employee creation with invalid data shows correct error messages
    Signed-off-by: Mahmooud Abdalhafeez <mahmoud99239@gmail.com>

--- feature/employee/src/test/kotlin/com/casecode/pos/feature/employee/EmployeeViewModelTest.kt
+++ feature/employee/src/test/kotlin/com/casecode/pos/feature/employee/EmployeeViewModelTest.kt
@@ -15,193 +15,204 @@
  */
 package com.casecode.pos.feature.employee
 
-import com.casecode.pos.core.domain.usecase.old.AddEmployeeUseCase
-import com.casecode.pos.core.domain.usecase.old.DeleteEmployeeUseCase
-import com.casecode.pos.core.domain.usecase.old.GetBusinessUseCase
-import com.casecode.pos.core.domain.usecase.old.GetEmployeesBusinessUseCase
-import com.casecode.pos.core.domain.usecase.old.UpdateEmployeesUseCase
-import com.casecode.pos.core.model.users.Employee
-import com.casecode.pos.core.testing.repository.TestAuthRepositoryO
-import com.casecode.pos.core.testing.repository.TestBusinessRepository
-import com.casecode.pos.core.testing.repository.TestEmployeesBusinessRepository
+import com.casecode.pos.core.domain.usecase.CreateEmployeeUseCase
+import com.casecode.pos.core.domain.usecase.DeleteEmployeeUseCase
+import com.casecode.pos.core.domain.usecase.GetBranchesUseCase
+import com.casecode.pos.core.domain.usecase.GetCurrentSessionUseCase
+import com.casecode.pos.core.domain.usecase.GetEmployeesUseCase
+import com.casecode.pos.core.domain.usecase.UpdateEmployeeUseCase
+import com.casecode.pos.core.model.SessionStateResult
+import com.casecode.pos.core.model.business.EmployeeRole
+import com.casecode.pos.core.testing.repository.business.TestBranchRepository
+import com.casecode.pos.core.testing.repository.business.TestEmployeeRepository
+import com.casecode.pos.core.testing.repository.business.TestSessionRepository
 import com.casecode.pos.core.testing.util.MainDispatcherRule
-import com.casecode.pos.core.testing.util.TestNetworkMonitor
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
-import kotlin.test.Test
+import org.junit.Test
+import java.io.IOException
 import kotlin.test.assertEquals
-import com.casecode.pos.core.data.R.string as dataString
 import com.casecode.pos.core.ui.R.string as uiString
 
 class EmployeeViewModelTest {
+
     @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
 
-    // Subject under test
+    // SUT
     private lateinit var viewModel: EmployeeViewModel
-    private val networkMonitor = TestNetworkMonitor()
-    private val employeesBusinessRepository = TestEmployeesBusinessRepository()
-    private val testAuth = TestAuthRepositoryO()
-    private val businessRepository = TestBusinessRepository()
-    private val getEmployees = GetEmployeesBusinessUseCase(employeesBusinessRepository)
-    private val getBusiness = GetBusinessUseCase(businessRepository)
-    private val addEmployee = AddEmployeeUseCase(employeesBusinessRepository)
-    private val updateEmployee = UpdateEmployeesUseCase(employeesBusinessRepository)
-    private val deleteEmployeeUseCase = DeleteEmployeeUseCase(employeesBusinessRepository)
-    private val employee = Employee("mahmoud22", "131434", "1234", "branch1", "sale")
+
+    // Test Doubles
+    private lateinit var employeeRepository: TestEmployeeRepository
+    private lateinit var branchRepository: TestBranchRepository
+    private lateinit var sessionRepository: TestSessionRepository
+
+    // Real Use Cases with Test Doubles
+    private lateinit var createEmployeeUseCase: CreateEmployeeUseCase
+    private lateinit var updateEmployeeUseCase: UpdateEmployeeUseCase
+    private lateinit var getEmployeesUseCase: GetEmployeesUseCase
+    private lateinit var deleteEmployeeUseCase: DeleteEmployeeUseCase
+    private lateinit var getBranchesUseCase: GetBranchesUseCase
+    private lateinit var getCurrentSessionUseCase: GetCurrentSessionUseCase
+    private val ownerSession =
+        SessionStateResult.OwnerLoggedIn("business1", "branch22", "122", "mahmoud")
 
     @Before
-    fun init() {
-        viewModel =
-            EmployeeViewModel(
-                networkMonitor,
-                getEmployees,
-                getBusiness,
-                addEmployee,
-                updateEmployee,
-                deleteEmployeeUseCase,
-                testAuth,
-            )
+    fun setup() {
+        employeeRepository = TestEmployeeRepository()
+        branchRepository = TestBranchRepository()
+        sessionRepository = TestSessionRepository()
+
+        getEmployeesUseCase = GetEmployeesUseCase(employeeRepository)
+        getBranchesUseCase = GetBranchesUseCase(branchRepository)
+        getCurrentSessionUseCase = GetCurrentSessionUseCase(sessionRepository)
+        createEmployeeUseCase = CreateEmployeeUseCase(
+            employeeRepository,
+            getCurrentSessionUseCase = getCurrentSessionUseCase,
+        )
+        updateEmployeeUseCase = UpdateEmployeeUseCase(
+            employeeRepository,
+            getCurrentSessionUseCase = getCurrentSessionUseCase,
+        )
+        deleteEmployeeUseCase = DeleteEmployeeUseCase(
+            employeeRepository,
+            getCurrentSessionUseCase = getCurrentSessionUseCase,
+        )
+        viewModel = EmployeeViewModel(
+            createEmployeeUseCase = createEmployeeUseCase,
+            getEmployeesUseCase = getEmployeesUseCase,
+            getBranchesUseCase = getBranchesUseCase,
+            updateEmployeeUseCase = updateEmployeeUseCase,
+            deleteEmployeeUseCase = deleteEmployeeUseCase,
+        )
     }
 
     @Test
-    fun addEmployee_hasEmployee_returnAddEmployeeMessage() = runTest {
-        // Given
-        viewModel.addEmployee(employee)
+    fun createEmployee_whenValidDataAndActiveSession_returnMessageSuccess() = runTest {
+        backgroundScope.launch(UnconfinedTestDispatcher()) { viewModel.uiState.collect() }
+
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeNameChanged("employee11"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeePhoneChanged("1212321312"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeePasswordChanged("123456666"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeRoleChanged(EmployeeRole.CASHIER))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeBranchAssigned("branch1"))
+        sessionRepository.setSessionInfo(ownerSession)
+
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.SaveClicked)
+
         assertEquals(
-            viewModel.uiState.value.userMessage,
             uiString.core_ui_success_add_employee_message,
+            viewModel.uiState.value.userMessage,
         )
     }
 
     @Test
-    fun addEmployee_nameEmployeeDuplicateEmployee_returnMessageDuplicateName() = runTest {
-        // Given
-        val nameDuplicate = employeesBusinessRepository.fakeEmployees[0].name
-        viewModel.addEmployee(Employee(nameDuplicate, "131434", "1234", "branch1", "sale"))
+    fun createEmployee_whenValidDataAndNoActiveSession_returnMessageFailure() = runTest {
+        backgroundScope.launch(UnconfinedTestDispatcher()) { viewModel.uiState.collect() }
+
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeNameChanged("employee11"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeePhoneChanged("1212321312"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeePasswordChanged("123456666"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeRoleChanged(EmployeeRole.CASHIER))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeBranchAssigned("branch1"))
+
+        sessionRepository.setSessionInfo(SessionStateResult.None)
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.SaveClicked)
+
         assertEquals(
+            uiString.core_ui_error_no_active_session,
+
             viewModel.uiState.value.userMessage,
+        )
+    }
+
+    @Test
+    fun createEmployee_whenNameDuplicate_returnMessageFailure() = runTest {
+        backgroundScope.launch(UnconfinedTestDispatcher()) { viewModel.uiState.collect() }
+
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeNameChanged("John Doe"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeePhoneChanged("1212321312"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeePasswordChanged("123456666"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeRoleChanged(EmployeeRole.CASHIER))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeBranchAssigned("branch1"))
+
+        sessionRepository.setSessionInfo(ownerSession)
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.SaveClicked)
+
+        assertEquals(
             uiString.core_ui_error_employee_name_duplicate,
+
+            viewModel.uiState.value.userMessage,
         )
     }
 
     @Test
-    fun addEmployee_error_returnMessageError() = runTest {
-        employeesBusinessRepository setReturnError true
-        viewModel.addEmployee(employee)
+    fun createEmployee_whenInvalidPassword_returnMessageFailure() = runTest {
+        backgroundScope.launch(UnconfinedTestDispatcher()) { viewModel.uiState.collect() }
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeNameChanged("employee11"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeePhoneChanged("1212321312"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeePasswordChanged("1234"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeRoleChanged(EmployeeRole.CASHIER))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeBranchAssigned("branch1"))
+
+        sessionRepository.setSessionInfo(ownerSession)
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.SaveClicked)
+
         assertEquals(
-            viewModel.uiState.value.userMessage,
-            dataString.core_data_add_employees_business_failure,
+            uiString.core_ui_error_add_employee_password_less_than_six,
+            viewModel.employeeFormUiState.value.formErrors.passwordError,
         )
     }
 
     @Test
-    fun updateEmployee_hasChangeOldEmployee_returnMessageUpdateSuccess() = runTest {
-        // Given
-        viewModel.setEmployeeSelected(employeesBusinessRepository.fakeEmployees[0])
-        // When
-        viewModel.updateEmployee(employee)
+    fun createEmployee_whenInvalidData_returnMessageFailure() = runTest {
+        backgroundScope.launch(UnconfinedTestDispatcher()) { viewModel.uiState.collect() }
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeNameChanged(""))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeePhoneChanged(""))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeePasswordChanged("12345"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeRoleChanged(EmployeeRole.CASHIER))
+
+        sessionRepository.setSessionInfo(ownerSession)
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.SaveClicked)
         assertEquals(
-            viewModel.uiState.value.userMessage,
-            uiString.core_ui_success_update_employee_message,
+            uiString.core_ui_error_employee_name_empty,
+            viewModel.employeeFormUiState.value.formErrors.nameError,
+        )
+        assertEquals(
+            uiString.core_ui_error_phone_empty,
+            viewModel.employeeFormUiState.value.formErrors.phoneError,
+        )
+        assertEquals(
+            uiString.core_ui_error_add_employee_password_less_than_six,
+            viewModel.employeeFormUiState.value.formErrors.passwordError,
+        )
+        assertEquals(
+            uiString.core_ui_error_branch_name_empty,
+            viewModel.employeeFormUiState.value.formErrors.assignedBranchesError,
         )
     }
 
     @Test
-    fun updateEmployee_nameEmployeeDuplicateEmployee_returnMessageDuplicateName() = runTest {
-        // Given
-        viewModel.setEmployeeSelected(employeesBusinessRepository.fakeEmployees[0])
-        // When
-        val nameDuplicate = employeesBusinessRepository.fakeEmployees[1].name
-        viewModel.updateEmployee(Employee(nameDuplicate, "131434", "1234", "branch1", "sale"))
-        assertEquals(
-            viewModel.uiState.value.userMessage,
-            uiString.core_ui_error_employee_name_duplicate,
-        )
-    }
+    fun createEmployee_whenHasError_returnErrorMessage() = runTest {
+        backgroundScope.launch(UnconfinedTestDispatcher()) { viewModel.uiState.collect() }
 
-    @Test
-    fun updateEmployee_hasSameOldEmployee_returnMessageUpdateEmployeeFailed() = runTest {
-        // Given
-        viewModel.setEmployeeSelected(employeesBusinessRepository.fakeEmployees[0])
-        val name = employeesBusinessRepository.fakeEmployees[0].name
-        val phoneNumber = employeesBusinessRepository.fakeEmployees[0].phoneNumber
-        val password = employeesBusinessRepository.fakeEmployees[0].password!!
-        val branchName = employeesBusinessRepository.fakeEmployees[0].branchName!!
-        val permission = employeesBusinessRepository.fakeEmployees[0].permission
-        // When
-        val sameEmployee = Employee(name, phoneNumber, password, branchName, permission)
-        viewModel.updateEmployee(sameEmployee)
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeNameChanged("employee11"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeePhoneChanged("1212321312"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeePasswordChanged("123456666"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeRoleChanged(EmployeeRole.CASHIER))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.EmployeeBranchAssigned("branch1"))
+        sessionRepository.setSessionInfo(ownerSession)
+
+        employeeRepository.setFailure(IOException("network error"))
+        viewModel.onEventEmployeeForm(EmployeeFormEvent.SaveClicked)
 
         assertEquals(
+            uiString.core_ui_error_add_employee_message,
             viewModel.uiState.value.userMessage,
-            uiString.core_ui_error_update_employee_message,
-        )
-    }
-
-    @Test
-    fun updateEmployee_error_returnMessageError() = runTest {
-        employeesBusinessRepository setReturnError true
-        viewModel.setEmployeeSelected(employeesBusinessRepository.fakeEmployees[0])
-        viewModel.updateEmployee(employee)
-
-        assertEquals(
-            viewModel.uiState.value.userMessage,
-            dataString.core_data_employee_update_business_failure,
-        )
-    }
-
-    @Test
-    fun updateEmployee_network_unavailable_returnMessageError() = runTest {
-        // Given
-        viewModel.setEmployeeSelected(employeesBusinessRepository.fakeEmployees[0])
-        // When
-        networkMonitor.setConnected(false)
-        viewModel.updateEmployee(employee)
-        // Then
-        assertEquals(
-            viewModel.uiState.value.userMessage,
-            uiString.core_ui_error_network,
-        )
-    }
-
-    @Test
-    fun deleteEmployee_returnMessageSuccess() = runTest {
-        // Given
-        viewModel.setEmployeeSelected(employeesBusinessRepository.fakeEmployees[0])
-        // When
-        viewModel.deleteEmployee()
-
-        assertEquals(
-            viewModel.uiState.value.userMessage,
-            dataString.core_data_employee_delete_business_success,
-        )
-    }
-
-    @Test
-    fun deleteEmployee_whenError_returnMessageError() = runTest {
-        // Given
-        employeesBusinessRepository setReturnError true
-        viewModel.setEmployeeSelected(employeesBusinessRepository.fakeEmployees[0])
-        // When
-        viewModel.deleteEmployee()
-        assertEquals(
-            viewModel.uiState.value.userMessage,
-            dataString.core_data_employee_delete_business_failure,
-        )
-    }
-
-    @Test
-    fun deleteEmployee_whenNetworkUnavailable_returnMessageError() = runTest {
-        // Given
-        viewModel.setEmployeeSelected(employeesBusinessRepository.fakeEmployees[0])
-        // When
-        networkMonitor.setConnected(false)
-        viewModel.deleteEmployee()
-        assertEquals(
-            viewModel.uiState.value.userMessage,
-            uiString.core_ui_error_network,
         )
     }
 }
