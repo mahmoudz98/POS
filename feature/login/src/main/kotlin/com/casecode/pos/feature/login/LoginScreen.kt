cf43446434c2b86ec165f57a7f3ef5afdf044f48
Mahmooud Abdalhafeez <mahmoud99239@gmail.com>
2025-11-09 08:12:55 +0200

    fix import statement for hilt view model to match correct package
    Signed-off-by: Mahmooud Abdalhafeez <mahmoud99239@gmail.com>

--- feature/login/src/main/kotlin/com/casecode/pos/feature/login/LoginScreen.kt
+++ feature/login/src/main/kotlin/com/casecode/pos/feature/login/LoginScreen.kt
@@ -48,7 +48,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.window.core.layout.WindowSizeClass.Companion.HEIGHT_DP_MEDIUM_LOWER_BOUND
 import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
